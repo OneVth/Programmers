3ed4804ec33b1bf6a827020afafabe8974dc7e55
OneVth <onebrotravel@gmail.com>
2024-07-09 21:46:20 +0900

    Solution using Linq

--- Lv0/_2[a와 b 출력하기]/Solution/Program.cs
+++ Lv0/_2[a와 b 출력하기]/Solution/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Solution
 {
@@ -9,13 +10,18 @@ namespace Solution
             String[] s;
 
             Console.Clear();
-            s = Console.ReadLine().Split(' ');
+            /*s = Console.ReadLine().Split(' ');
 
             if (int.TryParse(s[0], out int a))
                 Console.WriteLine("a = {0}", a);
 
             if (int.TryParse(s[1], out int b))
-                Console.WriteLine("b = {0}", b);
+                Console.WriteLine("b = {0}", b);*/
+
+            int[] i = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Console.WriteLine("a = {0}", i[0]);
+            Console.WriteLine("b = {0}", i[1]);
         }
     }
 }
