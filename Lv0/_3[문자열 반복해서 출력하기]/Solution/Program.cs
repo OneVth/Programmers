c83c1f5e5b8abb2be7b663c8affe6d0c36190112
OneVth <onebrotravel@gmail.com>
2024-07-09 22:04:03 +0900

    Upload solution

--- Lv0/_3[문자열 반복해서 출력하기]/Solution/Program.cs
+++ Lv0/_3[문자열 반복해서 출력하기]/Solution/Program.cs
@@ -6,7 +6,18 @@ namespace Solution
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            String[] input;
+
+            Console.Clear();
+            input = Console.ReadLine().Split(' ');
+
+            String s1 = input[0];
+            int a = Int32.Parse(input[1]);
+
+            for (int i = 0; i < a; i++)
+            {
+                Console.Write(s1);
+            }
         }
     }
 }
