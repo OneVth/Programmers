409b8246a12f9ae867c9b6a4f8f4e8a373b9a31a
OneVth <onebrotravel@gmail.com>
2024-07-09 21:32:12 +0900

    Upload solution

--- Lv0/_2[a와 b 출력하기]/Solution/Program.cs
+++ Lv0/_2[a와 b 출력하기]/Solution/Program.cs
@@ -6,7 +6,16 @@ namespace Solution
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            String[] s;
+
+            Console.Clear();
+            s = Console.ReadLine().Split(' ');
+
+            if (int.TryParse(s[0], out int a))
+                Console.WriteLine("a = {0}", a);
+
+            if (int.TryParse(s[1], out int b))
+                Console.WriteLine("b = {0}", b);
         }
     }
 }
