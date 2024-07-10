5801339c3ea72c985b7a710e4369ab87876532ae
OneVth <onebrotravel@gmail.com>
2024-07-10 18:00:49 +0900

    Upload Solution

---
+++ Lv0/_10[문자열 겹쳐쓰기]/Soulution/Solution.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Soulution
+{
+    internal class Solution
+    {
+        public string solution(string my_string, string overwrite_string, int s)
+        {
+            string answer = "";
+            answer = my_string.Remove(s, overwrite_string.Length);
+            answer = answer.Insert(s, overwrite_string);
+            return answer;
+        }
+    }
+}
