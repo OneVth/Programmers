c8b89cef902801b57062c83064ea0f0bc7bae3c8
OneVth <onebrotravel@gmail.com>
2024-07-10 17:56:48 +0900

    Upload solution

--- Lv0/_09[홀짝 구분하기]/Solution/Program.cs
+++ Lv0/_09[홀짝 구분하기]/Solution/Program.cs
@@ -6,7 +6,14 @@ namespace Solution
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            String[] s;
+
+            Console.Clear();
+            s = Console.ReadLine().Split(' ');
+
+            int a = Int32.Parse(s[0]);
+
+            Console.WriteLine((a % 2 == 0) ? $"{a} is even" : $"{a} is odd");
         }
     }
 }
