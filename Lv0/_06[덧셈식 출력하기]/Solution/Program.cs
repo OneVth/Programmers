e4841c7bde7586972f04ca23f3fc37a613993288
OneVth <onebrotravel@gmail.com>
2024-07-10 17:50:38 +0900

    Upload solution

--- Lv0/_06[덧셈식 출력하기]/Solution/Program.cs
+++ Lv0/_06[덧셈식 출력하기]/Solution/Program.cs
@@ -6,7 +6,16 @@ namespace Solution
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            String[] s;
+
+            Console.Clear();
+            s = Console.ReadLine().Split(' ');
+
+            int a = Int32.Parse(s[0]);
+            int b = Int32.Parse(s[1]);
+
+            Console.Write(a + " + " + b + " = ");
+            Console.WriteLine("{0}", a + b);
         }
     }
 }
