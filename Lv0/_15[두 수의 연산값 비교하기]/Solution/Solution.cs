343373bfcc9c17d266d75741688d07c331b803b6
OneVth <onebrotravel@gmail.com>
2024-07-11 15:04:21 +0900

    Upload solution

---
+++ Lv0/_15[두 수의 연산값 비교하기]/Solution/Solution.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(int a, int b)
+        {
+            int answer = 0;
+
+            int num1 = int.Parse($"{a}{b}");
+            int num2 = 2 * a * b;
+
+            answer = Math.Max(num1, num2);
+            return answer;
+        }
+    }
+}
