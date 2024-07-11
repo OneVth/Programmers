cdd745cf9760f37f925796d7a15c199ba5a35d07
OneVth <onebrotravel@gmail.com>
2024-07-11 14:56:41 +0900

    Update solution

--- Lv0/_13[문자열 곱하기]/Solution/Solution.cs
+++ Lv0/_13[문자열 곱하기]/Solution/Solution.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace Solution
 {
     internal class Solution
     {
-        public string solution(string my_string, int k)
+        public string solution1(string my_string, int k)
         {
             string answer = "";
             for (int i = 0; i < k; i++)
@@ -14,6 +15,17 @@ namespace Solution
                 answer += my_string;
             }
             return answer;
+        }
+
+        public string solution2(string my_string, int k)
+        {
+            String answer = String.Concat(Enumerable.Repeat(my_string, k));
+            return answer;
+        }
+
+        public string solution3(string my_string, int k)
+        {
+            return Enumerable.Range(0, k).Select(s => my_string).Aggregate((a, b) => a + b);
         }
     }
 }
