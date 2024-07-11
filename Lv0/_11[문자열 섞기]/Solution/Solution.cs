0333e5f56608e7814de238679401a60d1d0fc1bf
OneVth <onebrotravel@gmail.com>
2024-07-11 14:39:51 +0900

    Upload solution

---
+++ Lv0/_11[문자열 섞기]/Solution/Solution.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public string solution(string str1, string str2)
+        {
+            string answer = "";
+            char[] chars1 = str1.ToCharArray();
+            char[] chars2 = str2.ToCharArray();
+
+            for (int i = 0; i < str1.Length; i++)
+            {
+                answer += chars1[i];
+                answer += chars2[i];
+            }
+            return answer;
+        }
+    }
+}
