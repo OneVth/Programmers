1124b064de0fa92e70708d66a377412ddc7d4710
OneVth <onebrotravel@gmail.com>
2024-07-11 14:50:01 +0900

    Upload solution

---
+++ Lv0/_13[문자열 곱하기]/Solution/Solution.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public string solution(string my_string, int k)
+        {
+            string answer = "";
+            for (int i = 0; i < k; i++)
+            {
+                answer += my_string;
+            }
+            return answer;
+        }
+    }
+}
