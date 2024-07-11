0abc76b53bb147f68729bc22cb3556dedf225440
OneVth <onebrotravel@gmail.com>
2024-07-11 14:47:16 +0900

    Upload solution

---
+++ Lv0/_12[문자열 리스트를 문자열로 변환하기]/Solution/Solution.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public string solution(string[] arr)
+        {
+            string answer = "";
+
+            foreach (string str in arr)
+            {
+                answer += str;
+            }
+            return answer;
+        }
+    }
+}
