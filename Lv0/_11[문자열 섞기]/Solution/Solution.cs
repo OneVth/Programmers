9aa5c6751a03d909add0d4d66d8c7f173133e08d
OneVth <onebrotravel@gmail.com>
2024-07-11 14:44:24 +0900

    Update solutions

--- Lv0/_11[문자열 섞기]/Solution/Solution.cs
+++ Lv0/_11[문자열 섞기]/Solution/Solution.cs
@@ -6,7 +6,7 @@ namespace Solution
 {
     internal class Solution
     {
-        public string solution(string str1, string str2)
+        public string solution1(string str1, string str2)
         {
             string answer = "";
             char[] chars1 = str1.ToCharArray();
@@ -17,6 +17,33 @@ namespace Solution
                 answer += chars1[i];
                 answer += chars2[i];
             }
+            return answer;
+        }
+
+        public string solution2(string str1, string str2)
+        {
+            string answer = "";
+            
+            for (int i = 0; i < str1.Length; i++)
+            {
+                answer += string.Format("{0}{1}", str1[i], str2[i]);
+            }
+            return answer;
+        }
+
+        public string solution3(string str1, string str2)
+        {
+            string answer = "";
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < str1.Length; i++)
+            {
+                sb.Append(str1[i]);
+                sb.Append(str2[i]);
+            }
+
+            answer = sb.ToString();
+
             return answer;
         }
     }
