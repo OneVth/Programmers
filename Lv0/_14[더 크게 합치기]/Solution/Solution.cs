5b8c17a31c6919922cbefcfd2a56abc403b035c5
OneVth <onebrotravel@gmail.com>
2024-07-11 15:00:05 +0900

    Upload solution

---
+++ Lv0/_14[더 크게 합치기]/Solution/Solution.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(int a, int b)
+        {
+            int answer = 0;
+
+            string sA = a.ToString();
+            string sB = b.ToString();
+
+            int ab = int.Parse(sA + sB);
+            int ba = int.Parse(sB + sA);
+
+            answer = (ab >= ba) ? ab : ba;
+            return answer;
+        }
+    }
+}
