ca97ae2d2ba4b9f6ccec8855f566fbb258df3af8
OneVth <onebrotravel@gmail.com>
2024-07-12 11:48:13 +0900

    Update solution

--- Lv0/_17[공배수]/Solution/Solution.cs
+++ Lv0/_17[공배수]/Solution/Solution.cs
@@ -6,13 +6,23 @@ namespace Solution
 {
     internal class Solution
     {
-        public int solution(int number, int n, int m)
+        public int solution1(int number, int n, int m)
         {
             int answer = 0;
 
             if (number % n == 0)
                 if (number % m == 0)
                     answer = 1;
+
+            return answer;
+        }
+
+        public int solution2(int number, int n, int m)
+        {
+            int answer = 0;
+
+            if (number % n == 0 && number % m == 0)
+                answer = 1;
 
             return answer;
         }
