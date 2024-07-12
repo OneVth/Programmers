7fe2d2666ac44ea379555e29be019e44867d31d8
OneVth <onebrotravel@gmail.com>
2024-07-12 11:59:51 +0900

    Upload solution

---
+++ Lv0/_20[flag에 따라 다른 값 반환하기]/Solution/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(int a, int b, bool flag)
+        {
+            int answer = 0;
+            answer = flag ? a + b : a - b;
+            return answer;
+        }
+    }
+}
