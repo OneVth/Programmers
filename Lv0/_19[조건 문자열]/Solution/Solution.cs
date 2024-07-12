c63fc17aa607f525b4f77e5d961b545e48760a4c
OneVth <onebrotravel@gmail.com>
2024-07-12 11:58:53 +0900

    Update solution

--- Lv0/_19[조건 문자열]/Solution/Solution.cs
+++ Lv0/_19[조건 문자열]/Solution/Solution.cs
@@ -6,7 +6,7 @@ namespace Solution
 {
     internal class Solution
     {
-        public int solution(string ineq, string eq, int n, int m)
+        public int solution1(string ineq, string eq, int n, int m)
         {
             int answer = 0;
 
@@ -23,6 +23,44 @@ namespace Solution
                 default:
                     break;
             }
+
+            return answer;
+        }
+
+        public int solution2(string ineq, string eq, int n, int m)
+        {
+            int answer = 0;
+
+            switch (ineq + eq)
+            {
+                case "<=":
+                    answer = n <= m ? 1 : 0;
+                    break;
+                case "<!":
+                    answer = n < m ? 1 : 0;
+                    break;
+                case ">=":
+                    answer = n >= m ? 1 : 0;
+                    break;
+                case ">!":
+                    answer = n > m ? 1 : 0;
+                    break;
+                default:
+                    break;
+            }
+
+            return answer;
+        }
+
+        public int solution3(string ineq, string eq, int n, int m)
+        {
+            int answer = 0;
+            string ie = ineq + eq;
+
+            if (ie == ">=") return n >= m ? 1 : 0;
+            if (ie == ">!") return n > m ? 1 : 0;
+            if (ie == "<=") return n <= m ? 1 : 0;
+            if (ie == "<!") return n < m ? 1 : 0;
 
             return answer;
         }
