61e8e2a333d46738ebe3625dc84adfe96a2c4d4b
OneVth <onebrotravel@gmail.com>
2024-07-12 11:53:33 +0900

    Upload solution

---
+++ Lv0/_19[조건 문자열]/Solution/Solution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(string ineq, string eq, int n, int m)
+        {
+            int answer = 0;
+
+            switch (ineq)
+            {
+                case "<":
+                    if (eq == "=") answer = n <= m ? 1 : 0;
+                    else if (eq == "!") answer = n < m ? 1 : 0;
+                    break;
+                case ">":
+                    if (eq == "=") answer = n >= m ? 1 : 0;
+                    else if (eq == "!") answer = n > m ? 1 : 0;
+                    break;
+                default:
+                    break;
+            }
+
+            return answer;
+        }
+    }
+}
