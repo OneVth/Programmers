16dd02dd9b1b369254ded8e4097de3cffd3ae2a8
OneVth <onebrotravel@gmail.com>
2024-07-13 16:03:57 +0900

    Upload solution

--- Lv0/_25[이어 붙인 수]/Solution/Solution.cs
+++ Lv0/_25[이어 붙인 수]/Solution/Solution.cs
@@ -8,5 +8,22 @@ namespace Solution
 {
     internal class Solution
     {
+        public int solution(int[] num_list)
+        {
+            int answer = 0;
+            StringBuilder sbEven = new StringBuilder();
+            StringBuilder sbOdd = new StringBuilder();
+
+            for (int i = 0; i < num_list.Length; i++)
+            {
+                if (num_list[i] % 2 == 0) sbEven.Append(num_list[i]);
+                else sbOdd.Append(num_list[i]);
+            }
+
+            string even = sbEven.ToString();
+            string odd = sbOdd.ToString();
+
+            return int.Parse(even) + int.Parse(odd);
+        }
     }
 }
