1c78f8dc6031182baef4a8dc62f18f29c96757e1
OneVth <onebrotravel@gmail.com>
2024-07-13 15:52:09 +0900

    Upload solution

--- Lv0/_24[원소들의 곱과 합]/Solution/Solution.cs
+++ Lv0/_24[원소들의 곱과 합]/Solution/Solution.cs
@@ -8,5 +8,19 @@ namespace Solution
 {
     internal class Solution
     {
+        public int solution(int[] num_list)
+        {
+
+            int product = 1;
+            int sum = 0;
+
+            foreach (var n in num_list)
+            {
+                sum += n;
+                product *= n;
+            }
+
+            return Math.Pow(sum, 2) >= product ? 1 : 0;
+        }
     }
 }
