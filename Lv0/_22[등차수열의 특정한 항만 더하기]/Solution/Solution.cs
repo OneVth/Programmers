64dd314a444afdbdae1ce0fe3bb5741fd0632bc9
OneVth <onebrotravel@gmail.com>
2024-07-13 14:27:18 +0900

    Upload solution

--- Lv0/_22[등차수열의 특정한 항만 더하기]/Solution/Solution.cs
+++ Lv0/_22[등차수열의 특정한 항만 더하기]/Solution/Solution.cs
@@ -8,5 +8,15 @@ namespace Solution
 {
     internal class Solution
     {
+        public int solution(int a, int d, bool[] included)
+        {
+            int answer = 0;
+
+            for (int i = 0; i < included.Length; i++)
+            {
+                answer += included[i] ? a + i * d : 0;
+            }
+            return answer;
+        }
     }
 }
