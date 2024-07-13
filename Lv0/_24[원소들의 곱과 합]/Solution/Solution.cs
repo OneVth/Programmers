fcb5f39c1832c9f70022a0dbbe06a160a989850b
OneVth <onebrotravel@gmail.com>
2024-07-13 16:01:42 +0900

    Update solution

--- Lv0/_24[원소들의 곱과 합]/Solution/Solution.cs
+++ Lv0/_24[원소들의 곱과 합]/Solution/Solution.cs
@@ -8,9 +8,8 @@ namespace Solution
 {
     internal class Solution
     {
-        public int solution(int[] num_list)
+        public int solution1(int[] num_list)
         {
-
             int product = 1;
             int sum = 0;
 
@@ -21,6 +20,15 @@ namespace Solution
             }
 
             return Math.Pow(sum, 2) >= product ? 1 : 0;
+        }
+
+        public int solution2(int[] num_list)
+        {
+            int answer = 0;
+
+            answer = num_list.Aggregate(1, (x, y) => x * y) < Math.Pow(num_list.Sum(), 2) ? 1 : 0;
+
+            return answer;
         }
     }
 }
