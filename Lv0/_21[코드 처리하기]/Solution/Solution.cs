354c91170b5c69a163d5b07631bc536fd2f39278
OneVth <onebrotravel@gmail.com>
2024-07-13 14:15:13 +0900

    Upload solution

--- Lv0/_21[코드 처리하기]/Solution/Solution.cs
+++ Lv0/_21[코드 처리하기]/Solution/Solution.cs
@@ -8,5 +8,33 @@ namespace Solution
 {
     internal class Solution
     {
+        public string solution(string code)
+        {
+            string ret = "";
+            int mode = 0;
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < code.Length; i++)
+            {
+                switch (mode)
+                {
+                    case 0:
+                        if (code[i] == '1') mode = 1;
+                        else
+                            if (i % 2 == 0) sb.Append(code[i]);
+                        break;
+                    case 1:
+                        if (code[i] == '1') mode = 0;
+                        else
+                            if (i % 2 == 1) sb.Append(code[i]);
+                        break;
+                }
+            }
+
+            ret = sb.ToString();
+
+            return string.IsNullOrEmpty(ret) ? "EMPTY" : ret;
+        }
     }
 }
