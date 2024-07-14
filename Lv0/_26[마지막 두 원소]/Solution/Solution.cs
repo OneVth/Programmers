307cd0da441e5920195fb7316b1c86403d4dedc9
OneVth <onebrotravel@gmail.com>
2024-07-14 18:34:37 +0900

    Upload solution

---
+++ Lv0/_26[마지막 두 원소]/Solution/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int[] solution(int[] num_list)
+        {
+
+            int length = num_list.Length;
+            int[] answer = new int[length + 1];
+
+            for (int i = 0; i < length; i++)
+            {
+                answer[i] = num_list[i];
+            }
+
+            answer[length] = num_list[length - 1] > num_list[length - 2] ? 
+                num_list[length - 1] - num_list[length - 2] : 
+                num_list[length - 1] * 2;
+
+            return answer;
+        }
+    }
+}
