0e3c20063781fde6ea5f84faf3fc44b28a9fa607
OneVth <onebrotravel@gmail.com>
2024-07-14 21:12:29 +0900

    Upload solution

---
+++ Lv0/_30[수열과 구간 쿼리 2]/solution/Solution.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace solution
+{
+    internal class Solution
+    {
+        public int[] solution(int[] arr, int[,] queries)
+        {
+
+            int queryCount = queries.GetLength(0);
+            int[] answer = new int[queryCount];
+
+            for (int i = 0; i < queryCount; i++)
+            {
+                int s = queries[i, 0];
+                int e = queries[i, 1];
+                int k = queries[i, 2];
+                int[] temp = new int[e - s + 1];
+
+                for (int j = s, t = 0; j <= e; j++, t++)
+                {
+                    temp[t] = arr[j];
+                }
+
+                var filteredTemp = temp.Where(n => n > k).ToArray();
+                answer[i] = filteredTemp.Length == 0 ? -1 : filteredTemp.Min();
+            }
+
+            return answer;
+        }
+
+        
+    }
+}
