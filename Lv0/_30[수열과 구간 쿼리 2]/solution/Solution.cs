7cebdb53ece9cec2a2ac3571e3f5528ed14ea000
OneVth <onebrotravel@gmail.com>
2024-07-14 21:12:47 +0900

    Update solution

--- Lv0/_30[수열과 구간 쿼리 2]/solution/Solution.cs
+++ Lv0/_30[수열과 구간 쿼리 2]/solution/Solution.cs
@@ -8,7 +8,7 @@ namespace solution
 {
     internal class Solution
     {
-        public int[] solution(int[] arr, int[,] queries)
+        public int[] solution1(int[] arr, int[,] queries)
         {
 
             int queryCount = queries.GetLength(0);
@@ -33,6 +33,25 @@ namespace solution
             return answer;
         }
 
-        
+        public int[] solution2(int[] arr, int[,] queries)
+        {
+            int[] answer = new int[queries.GetLength(0)];
+            for (int i = 0; i < queries.GetLength(0); i++)
+            {
+                int s = queries[i, 0];
+                int e = queries[i, 1];
+                int k = queries[i, 2];
+
+                int num = arr.Skip(s)
+                    .Take(e - s + 1)
+                    .Where(w => w > k)
+                    .DefaultIfEmpty(-1)
+                    .OrderBy(o => o)
+                    .First();
+
+                answer[i] = num;
+            }
+            return answer;
+        }
     }
 }
