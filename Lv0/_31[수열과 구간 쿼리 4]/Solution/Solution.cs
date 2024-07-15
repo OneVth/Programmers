ceeda44175737a376e971c2964b79a604a050bd3
OneVth <onebrotravel@gmail.com>
2024-07-15 13:14:55 +0900

    Upload solution

---
+++ Lv0/_31[수열과 구간 쿼리 4]/Solution/Solution.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int[] solution(int[] arr)
+        {
+            int[] stk = new int[] { };
+            int i = 0;
+            List<int> temp = new List<int>();
+
+            while (i < arr.Length)
+            {
+                if (temp.Count == 0)
+                {
+                    temp.Add(arr[i]);
+                    i++;
+                }
+                else
+                {
+                    if (temp[temp.Count - 1] < arr[i])
+                    {
+                        temp.Add(arr[i]);
+                        i++;
+                    }
+                    else
+                    {
+                        temp.RemoveAt(temp.Count - 1);
+                    }
+                }
+            }
+
+            stk = temp.ToArray();
+            return stk;
+        }
+    }
+}
