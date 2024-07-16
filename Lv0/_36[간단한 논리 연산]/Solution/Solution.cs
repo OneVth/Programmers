1c98202da8e774c6df154ba64353ffea6d61058f
OneVth <onebrotravel@gmail.com>
2024-07-16 14:51:18 +0900

    Upload solution

---
+++ Lv0/_36[간단한 논리 연산]/Solution/Solution.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public bool solution(bool x1, bool x2, bool x3, bool x4)
+        {
+
+            return (x1 || x2) && (x3 || x4);
+        }
+    }
+}
