d6e91fd283f5a2532dfc912fd7e56f363722f854
OneVth <onebrotravel@gmail.com>
2024-07-16 15:12:51 +0900

    Upload solution

---
+++ Lv0/_38[글자 이어 붙여 문자열 만들기]/Solution/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public string solution(string my_string, int[] index_list)
+        {
+            string answer = "";
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < index_list.Length; i++)
+            {
+                sb.Append(my_string[index_list[i]]);
+            }
+
+            answer = sb.ToString();
+            return answer;
+        }
+    }
+}
