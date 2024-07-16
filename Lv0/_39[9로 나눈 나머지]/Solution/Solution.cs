e3a71c788b5667f6d8ed6fb040f4ab6c47a43173
OneVth <onebrotravel@gmail.com>
2024-07-16 15:17:47 +0900

    Update solution

---
+++ Lv0/_39[9로 나눈 나머지]/Solution/Solution.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(string number)
+        {
+            int answer = 0;
+            List<int> container = new List<int>();
+            foreach (char c in number)
+            {
+                container.Add(int.Parse(c.ToString()));
+            }
+            answer = container.Sum() % 9;
+            return answer;
+        }
+    }
+}
