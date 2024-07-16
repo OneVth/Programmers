7cb5fde89ff4c12e304ae7ca1a769dedbcae50b1
OneVth <onebrotravel@gmail.com>
2024-07-16 15:12:09 +0900

    Update solution

--- Lv0/_37[주사위 게임 3]/Solution/Solution.cs
+++ Lv0/_37[주사위 게임 3]/Solution/Solution.cs
@@ -8,7 +8,7 @@ namespace Solution
 {
     internal class Solution
     {
-        public int solution(int a, int b, int c, int d)
+        public int solution1(int a, int b, int c, int d)
         {
             int[] counts = new int[7];
             List<int> uniqueNumbers = new List<int>();
@@ -61,6 +61,61 @@ namespace Solution
             }
 
             if (frequencies.Count == 4) return Math.Min(Math.Min(a, b), Math.Min(c, d));
+
+            return 0;
+        }
+
+        public int solution2(int a, int b, int c, int d)
+        {
+            var list = new int[4] { a, b, c, d };
+
+            var arr = list.GroupBy(g => g)
+                .OrderByDescending(g => g.Count())
+                .Select(s => (Number: s.Key, Count: s.Count()))
+                .ToArray();
+
+            if (arr[0].Count == 4) return 1111 * arr[0].Number;
+            else if (arr[0].Count == 3) return (int)Math.Pow(10 * arr[0].Number + arr[1].Number, 2);
+            else if (arr[0].Count == 2)
+            {
+                if (arr[1].Count == 2) return (arr[0].Number + arr[1].Number) * Math.Abs(arr[0].Number - arr[1].Number);
+                else return arr[1].Number * arr[2].Number;
+            }
+            else return list.Min();
+        }
+
+        public int solution3(int a, int b, int c, int d)
+        {
+            List<int> numbers = new List<int>() { a, b, c, d };
+            var groups = numbers.GroupBy(n => n).ToList();
+
+            if (groups.Count == 1) return 1111 * a;
+
+            if(groups.Count == 2)
+            {
+                if(groups.Any(g => g.Count() == 3))
+                {
+                    int p = groups.First(g => g.Count() == 3).Key;
+                    int q = groups.First(g => g.Count() == 1).Key;
+                    return (int)Math.Pow(10 * p + q, 2);
+                }
+                else
+                {
+                    int p = groups[0].Key;
+                    int q = groups[1].Key;
+                    return (p + q) * Math.Abs(p - q);
+                }
+            }
+
+            if(groups.Count == 3)
+            {
+                int p = groups.First(g => g.Count() == 2).Key;
+                int q = groups.First(g => g.Count() == 1).Key;
+                int r = groups.First(g => g.Count() == 1).Key;
+                return q * r;
+            }
+
+            if (groups.Count == 4) return numbers.Min();
 
             return 0;
         }
