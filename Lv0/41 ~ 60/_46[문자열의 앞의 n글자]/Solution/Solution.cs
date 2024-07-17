5358405813e73b52706af61fbb851d6a40975535
OneVth <onebrotravel@gmail.com>
2024-07-18 08:30:34 +0900

    Upload solution

---
+++ Lv0/41 ~ 60/_46[문자열의 앞의 n글자]/Solution/Solution.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public string solution(string my_string, int n)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append(my_string.Substring(0, n));
+
+            return sb.ToString();
+        }
+    }
+}
