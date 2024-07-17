45ab88e71085aa7f926eeb77eb796f5282b9f30b
OneVth <onebrotravel@gmail.com>
2024-07-17 12:49:48 +0900

    Upload solution

---
+++ Lv0/41 ~ 60/_44[접미사 배열]/Solution/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public string[] solution(string my_string)
+        {
+            string[] answer = new string[] { };
+            List<string> strList = new List<string>();
+
+            for (int i = 0; i < my_string.Length; i++)
+            {
+                strList.Add(my_string.Substring(i));
+            }
+
+            answer = strList.OrderBy(o => o).ToArray();
+            return answer;
+        }
+    }
+}
