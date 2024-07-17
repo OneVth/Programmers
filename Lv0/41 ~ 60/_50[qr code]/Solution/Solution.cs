ffb456fcfb72cb11643b65a369471f8fc1b26f26
OneVth <onebrotravel@gmail.com>
2024-07-18 08:44:27 +0900

    Upload solution

---
+++ Lv0/41 ~ 60/_50[qr code]/Solution/Solution.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public string solution(int q, int r, string code)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < code.Length; i++)
+            {
+                if (i % q == r) sb.Append(code[i]);
+            }
+            return sb.ToString();
+        }
+    }
+}
