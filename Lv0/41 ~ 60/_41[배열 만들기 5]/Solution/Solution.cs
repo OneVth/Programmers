b83c0619892b8157f13b30ed0d53ce2fbeb1d472
OneVth <onebrotravel@gmail.com>
2024-07-17 12:41:48 +0900

    Upload solution

---
+++ Lv0/41 ~ 60/_41[배열 만들기 5]/Solution/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int[] solution(string[] intStrs, int k, int s, int l)
+        {
+            int[] answer;
+            List<int> list = new List<int>();
+
+            foreach (string str in intStrs)
+            {
+                char[] intChars = str.ToCharArray(s, l);
+                int targetInt = int.Parse(intChars);
+                if (targetInt > k) list.Add(targetInt);
+            }
+
+            answer = list.ToArray();
+            return answer;
+        }
+    }
+}
