527e369f0e2dac370019e85e8c7875a714e4a40f
OneVth <onebrotravel@gmail.com>
2024-07-17 12:46:31 +0900

    Upload solution

---
+++ Lv0/41 ~ 60/_43[문자열 뒤의 n글자]/Solution/Solution.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public string solution(string my_string, int n)
+        {
+            string answer = "";
+            int length = my_string.Length;
+
+            answer += my_string.Substring(length - n);
+
+            return answer;
+        }
+    }
+}
