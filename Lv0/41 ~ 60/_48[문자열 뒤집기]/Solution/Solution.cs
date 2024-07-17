a847a52c3b0deb5522e3da693d218228e264bd17
OneVth <onebrotravel@gmail.com>
2024-07-18 08:38:18 +0900

    Upload solution

---
+++ Lv0/41 ~ 60/_48[문자열 뒤집기]/Solution/Solution.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public string solution(string my_string, int s, int e)
+        {
+            char[] chars = my_string.ToCharArray();
+
+            Array.Reverse(chars, s, e - s + 1);
+
+            return new string(chars);
+        }
+    }
+}
