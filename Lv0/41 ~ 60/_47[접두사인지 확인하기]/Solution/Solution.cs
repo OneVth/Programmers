0bac3c3cd06af81b273e0de4f2990c62d97c6f2a
OneVth <onebrotravel@gmail.com>
2024-07-18 08:33:36 +0900

    Upload solution

---
+++ Lv0/41 ~ 60/_47[접두사인지 확인하기]/Solution/Solution.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(string my_string, string is_prefix)
+        {
+            List<string> prefix = new List<string>();
+
+            for (int i = 0; i < my_string.Length; i++)
+            {
+                prefix.Add(my_string.Substring(0, i));
+            }
+
+            return prefix.Contains(is_prefix) ? 1 : 0;
+        }
+    }
+}
