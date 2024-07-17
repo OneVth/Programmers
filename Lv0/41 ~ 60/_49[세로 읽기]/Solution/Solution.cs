979add7b20a8bd8d28af30d3d530f65ff59d2812
OneVth <onebrotravel@gmail.com>
2024-07-18 08:39:03 +0900

    Upload solution

---
+++ Lv0/41 ~ 60/_49[세로 읽기]/Solution/Solution.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public string solution(string my_string, int m, int c)
+        {
+            List<char> list = new List<char>();
+
+            for (int i = c - 1; i < my_string.Length; i += m)
+            {
+                list.Add(my_string[i]);
+            }
+
+            return new string(list.ToArray());
+        }
+    }
+}
