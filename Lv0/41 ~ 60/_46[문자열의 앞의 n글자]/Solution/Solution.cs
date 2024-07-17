cfa1bd0c1f22164dd1e01931f56b4ce1c8afc1f8
OneVth <onebrotravel@gmail.com>
2024-07-18 08:32:47 +0900

    Update solution

--- Lv0/41 ~ 60/_46[문자열의 앞의 n글자]/Solution/Solution.cs
+++ Lv0/41 ~ 60/_46[문자열의 앞의 n글자]/Solution/Solution.cs
@@ -8,13 +8,18 @@ namespace Solution
 {
     internal class Solution
     {
-        public string solution(string my_string, int n)
+        public string solution1(string my_string, int n)
         {
             StringBuilder sb = new StringBuilder();
 
             sb.Append(my_string.Substring(0, n));
 
             return sb.ToString();
+        }
+
+        public string solution2(string my_string, int n)
+        {
+            return my_string.Length == n ? my_string : my_string.Remove(n);
         }
     }
 }
