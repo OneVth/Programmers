7380c106d9b7e3cc1ea22139d808762a5cf665fc
OneVth <onebrotravel@gmail.com>
2024-07-17 13:11:35 +0900

    Upload solution

---
+++ Lv0/41 ~ 60/_45[접미사인지 확인하기]/Solution/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(string my_string, string is_suffix)
+        {
+            int answer = 0;
+            List<string> strList = new List<string>();
+
+            for (int i = 0; i < my_string.Length; i++)
+            {
+                strList.Add(my_string.Substring(i));
+            }
+
+            answer = strList.Contains(is_suffix) ? 1 : 0;
+            return answer;
+        }
+    }
+}
