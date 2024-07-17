61c99b6b0ffe7ca7661105f453c9231641876150
OneVth <onebrotravel@gmail.com>
2024-07-17 12:45:42 +0900

    Upload solution

---
+++ Lv0/41 ~ 60/_42[부분 문자열 이어 붙여 문자열 만들기]/Solution/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public string solution(string[] my_strings, int[,] parts)
+        {
+            string answer = "";
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < my_strings.Length; i++)
+            {
+                int s = parts[i, 0];
+                int e = parts[i, 1];
+
+                sb.Append(my_strings[i].Substring(s, e - s + 1));
+            }
+
+            answer = sb.ToString();
+            return answer;
+        }
+    }
+}
