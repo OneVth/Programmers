be13e4be7a1d1b5d5ee0f1952304eca69660019e
OneVth <onebrotravel@gmail.com>
2024-07-20 13:56:49 +0900

    Update solution

--- Lv0/41 ~ 60/_51[문자 개수 세기]/Soultion/Solutin.cs
+++ Lv0/41 ~ 60/_51[문자 개수 세기]/Soultion/Solutin.cs
@@ -8,7 +8,7 @@ namespace Soultion
 {
     internal class Solutin
     {
-        public int[] solution(string my_string)
+        public int[] solution1(string my_string)
         {
             char[] chars = new char[] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
             int[] answer = new int[chars.Length];
@@ -19,6 +19,22 @@ namespace Soultion
                 {
                     if (c == chars[i]) answer[i] += 1;
                 }
+            }
+
+            return answer;
+        }
+
+        public int[] solution2(string my_string)
+        {
+            // 52 : Uppercase(26) + lowercase(26) English letter
+            int[] answer = new int[52];
+
+            foreach(char c in my_string)
+            {
+                // 'A' has an ASCII value of 65
+                if (Char.IsUpper(c)) answer[c - 'A']++;
+                // 'a' has an ASCII value of 97
+                else answer[26 + c - 'a']++;
             }
 
             return answer;
