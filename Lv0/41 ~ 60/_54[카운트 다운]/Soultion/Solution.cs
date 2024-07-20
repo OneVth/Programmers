ba86ddb252c8dca72275d48c2a08ff06c44d95a2
OneVth <onebrotravel@gmail.com>
2024-07-20 14:27:51 +0900

    Upload solution

---
+++ Lv0/41 ~ 60/_54[카운트 다운]/Soultion/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soultion
+{
+    internal class Solution
+    {
+        public int[] solution(int start_num, int end_num)
+        {
+            return Enumerable.Range(end_num, start_num - end_num + 1).Reverse().ToArray();
+        }
+    }
+}
