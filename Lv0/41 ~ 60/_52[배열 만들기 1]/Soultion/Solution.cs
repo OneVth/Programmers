03129858c9a4654c73b130b396f03336a7b6280d
OneVth <onebrotravel@gmail.com>
2024-07-20 14:20:48 +0900

    Upload solution

---
+++ Lv0/41 ~ 60/_52[배열 만들기 1]/Soultion/Solution.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soultion
+{
+    internal class Solution
+    {
+        public int[] solution(int n, int k)
+        {
+            List<int> list = new List<int>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % k == 0) list.Add(i);
+            }
+
+            return list.ToArray();
+        }
+    }
+}
