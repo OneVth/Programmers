dcc814dfc39855154197b47f97878e469bf1b627
OneVth <onebrotravel@gmail.com>
2024-07-20 14:28:25 +0900

    Upload solution

---
+++ Lv0/41 ~ 60/_55[가까운 1 찾기]/Soultion/Solution.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soultion
+{
+    internal class Solution
+    {
+        public int solution(int[] arr, int idx)
+        {
+
+            for (int i = idx; i < arr.Length; i++)
+            {
+                if (arr[i] == 1) return i;
+            }
+
+            return -1;
+        }
+    }
+}
