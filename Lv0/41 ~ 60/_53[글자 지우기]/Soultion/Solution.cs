c4e7e193d6833cf35fb0fd2c363cc3022c90dc35
OneVth <onebrotravel@gmail.com>
2024-07-20 14:22:35 +0900

    Upload solution

---
+++ Lv0/41 ~ 60/_53[글자 지우기]/Soultion/Solution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soultion
+{
+    internal class Solution
+    {
+        public string solution(string my_string, int[] indices)
+        {
+            StringBuilder sb = new StringBuilder(my_string);
+
+            Array.Sort(indices);
+            Array.Reverse(indices);
+
+            foreach (int i in indices)
+            {
+                sb.Remove(i, 1);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
