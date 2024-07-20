6c1eced3a555261fbe757d0cd3112f1623761b4c
OneVth <onebrotravel@gmail.com>
2024-07-20 13:50:05 +0900

    Upload solution

---
+++ Lv0/41 ~ 60/_51[문자 개수 세기]/Soultion/Solutin.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soultion
+{
+    internal class Solutin
+    {
+        public int[] solution(string my_string)
+        {
+            char[] chars = new char[] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+            int[] answer = new int[chars.Length];
+
+            foreach (char c in my_string)
+            {
+                for (int i = 0; i < chars.Length; i++)
+                {
+                    if (c == chars[i]) answer[i] += 1;
+                }
+            }
+
+            return answer;
+        }
+    }
+}
