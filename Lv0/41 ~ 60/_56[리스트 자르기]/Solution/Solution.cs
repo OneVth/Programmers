4390a1e4b4d8101df26e1b98f0eb88f8cd5c8825
OneVth <onebrotravel@gmail.com>
2024-07-21 13:35:06 +0900

    Update solution

--- Lv0/41 ~ 60/_56[리스트 자르기]/Solution/Solution.cs
+++ Lv0/41 ~ 60/_56[리스트 자르기]/Solution/Solution.cs
@@ -8,7 +8,7 @@ namespace Solution
 {
     internal class Solution
     {
-        public int[] solution(int n, int[] slicer, int[] num_list)
+        public int[] solution1(int n, int[] slicer, int[] num_list)
         {
             int[] answer = new int[] { };
             int a = slicer[0];
@@ -31,6 +31,39 @@ namespace Solution
                     break;
             }
             return answer;
+        }
+
+        public int[] solution2(int n, int[] slicer, int[] num_list)
+        {
+            int start = 0;
+            int end = num_list.Length - 1;
+            int term = 1;
+
+            if(n == 1) // 0번 인덱스부터 b번 인덱스까지
+            {
+                end = slicer[1];
+            }
+            else if(n == 2) // a번 인덱스부터 마지막 인덱스까지
+            {
+                start = slicer[0];
+            }
+            else if(n == 3) // a번 인덱스부터 b번 인덱스까지
+            {
+                start = slicer[0];
+                end = slicer[1];
+            }
+            else // a번 인덱스부터 b번 인덱스까지 c 간격으로
+            {
+                start = slicer[0];
+                end = slicer[1];
+                term = slicer[2];
+            }
+
+            var list = new List<int>();
+            for (int i = start; i <= end; i += term)
+                list.Add(num_list[i]);
+
+            return list.ToArray();
         }
     }
 }
