95d0152d5396c2679c18250d4d7356980a7e82e7
OneVth <onebrotravel@gmail.com>
2024-07-21 14:03:54 +0900

    Update solution

--- Lv0/41 ~ 60/_60[배열 조각하기]/Solution/Solution.cs
+++ Lv0/41 ~ 60/_60[배열 조각하기]/Solution/Solution.cs
@@ -8,7 +8,7 @@ namespace Solution
 {
     internal class Solution
     {
-        public int[] solution(int[] arr, int[] query)
+        public int[] solution1(int[] arr, int[] query)
         {
 
             for (int i = 0; i < query.Length; i++)
@@ -21,5 +21,19 @@ namespace Solution
 
             return arr;
         }
+
+        public int[] solution2(int[] arr, int[] query) {
+            int start = 0;
+            int end = arr.Length - 1;
+            for(int i = 0; i < query.Length; i++)
+            {
+                if (i % 2 == 0)
+                    end = start + query[i] - 1;
+                else
+                    start = start + query[i];
+            }
+
+            if (start > end) return new int[1] { -1 };
+            return arr.Skip(start).Take(end - start + 1).ToArray();
     }
 }
