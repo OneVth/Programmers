cd7d0c53aac69ff56d6f52a85c0138c6fa0d8500
OneVth <onebrotravel@gmail.com>
2024-07-21 13:54:16 +0900

    Update solution

--- Lv0/41 ~ 60/_59[2의 영역]/Solution/Solution.cs
+++ Lv0/41 ~ 60/_59[2의 영역]/Solution/Solution.cs
@@ -8,7 +8,7 @@ namespace Solution
 {
     internal class Solution
     {
-        public int[] solution(int[] arr)
+        public int[] solution1(int[] arr)
         {
             int[] answer = new int[] { };
             List<int> list = new List<int>();
@@ -23,6 +23,18 @@ namespace Solution
             else if (list.Count == 1) answer = new int[] { 2 };
             else answer = arr.Skip(list[0]).Take(list[list.Count - 1] - list[0] + 1).ToArray();
 
+            return answer;
+        }
+
+        public int[] solution2(int[] arr)
+        {
+            int start = Array.IndexOf(arr, 2);
+            if (start == -1) return new int[1] { -1 };
+
+            int end = Array.LastIndexOf(arr, 2);
+
+            int[] answer = new int[end - start + 1];
+            Array.Copy(arr, start, answer, 0, answer.Length);
             return answer;
         }
     }
