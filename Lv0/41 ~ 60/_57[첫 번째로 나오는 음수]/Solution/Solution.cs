44b04f0fe0a6918c169444399b00a61cf3566104
OneVth <onebrotravel@gmail.com>
2024-07-21 13:38:34 +0900

    Upload solution

---
+++ Lv0/41 ~ 60/_57[첫 번째로 나오는 음수]/Solution/Solution.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(int[] num_list)
+        {
+            for (int i = 0; i < num_list.Length; i++)
+                if (num_list[i] < 0) return i;
+
+            return -1;
+        }
+    }
+}
