0dae949a2cffaf8ed21b5b4d1e991cd7ac347686
OneVth <onebrotravel@gmail.com>
2024-07-21 13:50:58 +0900

    Update solution

--- Lv0/41 ~ 60/_58[배열 만들기 3]/Solution/Solution.cs
+++ Lv0/41 ~ 60/_58[배열 만들기 3]/Solution/Solution.cs
@@ -8,7 +8,7 @@ namespace Solution
 {
     internal class Solution
     {
-        public int[] solution(int[] arr, int[,] intervals)
+        public int[] solution1(int[] arr, int[,] intervals)
         {
             List<int> list = new List<int>();
 
@@ -21,6 +21,18 @@ namespace Solution
                     list.Add(arr[j]);
             }
             return list.ToArray();
+        }
+
+        public int[] solution2(int[] arr, int[,] intervals)
+        {
+            int size1 = intervals[0, 1] - intervals[0, 0] + 1;
+            int size2 = intervals[1, 1] - intervals[1, 0] + 1;
+
+            int[] answer = new int[size1 + size2];
+            Array.Copy(arr, intervals[0, 0], answer, 0, size1);
+            Array.Copy(arr, intervals[1, 0], answer, size1, size2);
+
+            return answer;
         }
     }
 }
