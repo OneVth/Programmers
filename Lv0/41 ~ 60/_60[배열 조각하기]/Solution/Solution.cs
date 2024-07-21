fd0f8cbc754ae7638b8a3f84bf04678ee90ec2a3
OneVth <onebrotravel@gmail.com>
2024-07-21 13:55:39 +0900

    Upload solution

---
+++ Lv0/41 ~ 60/_60[배열 조각하기]/Solution/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int[] solution(int[] arr, int[] query)
+        {
+
+            for (int i = 0; i < query.Length; i++)
+            {
+                if (i % 2 == 0)
+                    arr = arr.Take(query[i] + 1).ToArray();
+                else
+                    arr = arr.Skip(query[i]).ToArray();
+            }
+
+            return arr;
+        }
+    }
+}
