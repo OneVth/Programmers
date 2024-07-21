963f60ab89fbd052f82340906385abae9fece9a1
OneVth <onebrotravel@gmail.com>
2024-07-21 13:40:45 +0900

    Update solution

--- Lv0/41 ~ 60/_57[첫 번째로 나오는 음수]/Solution/Solution.cs
+++ Lv0/41 ~ 60/_57[첫 번째로 나오는 음수]/Solution/Solution.cs
@@ -8,12 +8,17 @@ namespace Solution
 {
     internal class Solution
     {
-        public int solution(int[] num_list)
+        public int solution1(int[] num_list)
         {
             for (int i = 0; i < num_list.Length; i++)
                 if (num_list[i] < 0) return i;
 
             return -1;
+        }
+
+        public int solution2(int[] num_list)
+        {
+            return Array.FindIndex(num_list, f => f < 0);
         }
     }
 }
