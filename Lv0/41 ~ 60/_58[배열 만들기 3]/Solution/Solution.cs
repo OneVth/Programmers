a427225d72b93d9756bdc291f1f808708ddb9973
OneVth <onebrotravel@gmail.com>
2024-07-21 13:43:37 +0900

    Upload solution

---
+++ Lv0/41 ~ 60/_58[배열 만들기 3]/Solution/Solution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int[] solution(int[] arr, int[,] intervals)
+        {
+            List<int> list = new List<int>();
+
+            for (int i = 0; i < intervals.GetLength(0); i++)
+            {
+                int start = intervals[i, 0];
+                int end = intervals[i, 1];
+
+                for (int j = start; j <= end; j++)
+                    list.Add(arr[j]);
+            }
+            return list.ToArray();
+        }
+    }
+}
