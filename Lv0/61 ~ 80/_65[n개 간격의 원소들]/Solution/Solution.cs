085b78251206cb70548da69e61eb96eb2918d1ee
OneVth <onebrotravel@gmail.com>
2024-07-22 10:58:40 +0900

    Upload solution

---
+++ Lv0/61 ~ 80/_65[n개 간격의 원소들]/Solution/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int[] solution(int[] num_list, int n)
+        {
+            return num_list.Where((w, index) => index % n == 0).ToArray();
+        }
+    }
+}
