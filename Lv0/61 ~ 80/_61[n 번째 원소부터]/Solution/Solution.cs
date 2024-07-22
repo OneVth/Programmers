666ba380c7205155f65550a0fbeef539b7595b6b
OneVth <onebrotravel@gmail.com>
2024-07-22 10:15:46 +0900

    Update solution

--- Lv0/61 ~ 80/_61[n 번째 원소부터]/Solution/Solution.cs
+++ Lv0/61 ~ 80/_61[n 번째 원소부터]/Solution/Solution.cs
@@ -8,9 +8,18 @@ namespace Solution
 {
     internal class Solution
     {
-        public int[] solution(int[] num_list, int n)
+        public int[] solution1(int[] num_list, int n)
         {
             return num_list.Skip(n - 1).ToArray();
+        }
+
+        public int[] solution2(int[] num_list, int n)
+        {
+            int[] answer = new int[num_list.Length - n + 1];
+
+            for (int i = n - 1, j = 0; i < num_list.Length; i++, j++) answer[j] = num_list[i];
+
+            return answer;
         }
     }
 }
