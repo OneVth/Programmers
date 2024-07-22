2060f3cdaf27667ad867461ac2e6ef949469026d
OneVth <onebrotravel@gmail.com>
2024-07-22 11:41:29 +0900

    Update solution

--- Lv0/61 ~ 80/_65[n개 간격의 원소들]/Solution/Solution.cs
+++ Lv0/61 ~ 80/_65[n개 간격의 원소들]/Solution/Solution.cs
@@ -8,9 +8,16 @@ namespace Solution
 {
     internal class Solution
     {
-        public int[] solution(int[] num_list, int n)
+        public int[] solution1(int[] num_list, int n)
         {
             return num_list.Where((w, index) => index % n == 0).ToArray();
+        }
+
+        public int[] solution2(int[] num_list, int n)
+        {
+            int[] answer = new int[(num_list.Length + n - 1) / n];
+            for (int i = 0; i < answer.Length; i++) answer[i] = num_list[i * n];
+            return answer;
         }
     }
 }
