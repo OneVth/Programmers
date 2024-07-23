d59a79c282c9b47fc172e93aff4ba8da6995d449
OneVth <onebrotravel@gmail.com>
2024-07-23 11:04:21 +0900

    Upload solution

---
+++ Lv0/61 ~ 80/_66[홀수 vs 짝수]/Solution/Solution.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(int[] num_list)
+        {
+
+            int oddSum = num_list.Where((w, index) => index % 2 == 0).Sum();
+            int evenSum = num_list.Where((w, index) => index % 2 == 1).Sum();
+
+            return oddSum >= evenSum ? oddSum : evenSum;
+        }
+    }
+}
