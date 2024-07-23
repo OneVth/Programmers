4825737531d48f8cdeed2fb9739fe4c8befe2878
OneVth <onebrotravel@gmail.com>
2024-07-23 11:10:47 +0900

    Upload solution

---
+++ Lv0/61 ~ 80/_68[할 일 목록]/Solution/Solution.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public string[] solution(string[] todo_list, bool[] finished)
+        {
+            List<string> list = new List<string>();
+
+            for (int i = 0; i < finished.Length; i++)
+                if (finished[i] == false)
+                    list.Add(todo_list[i]);
+
+            return list.ToArray();
+        }
+    }
+}
