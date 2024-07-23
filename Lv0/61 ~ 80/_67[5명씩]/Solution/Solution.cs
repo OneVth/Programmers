a5dec8004a2de5d1ffae5dfe93f53657baa69e4b
OneVth <onebrotravel@gmail.com>
2024-07-23 11:08:29 +0900

    Upload solution

---
+++ Lv0/61 ~ 80/_67[5명씩]/Solution/Solution.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public string[] solution(string[] names)
+        {
+            int length = (names.Length + 5 - 1) / 5;
+            string[] answer = new string[length];
+
+            for (int i = 0; i < length; i++)
+                answer[i] = names[i * 5];
+
+            return answer;
+        }
+    }
+}
