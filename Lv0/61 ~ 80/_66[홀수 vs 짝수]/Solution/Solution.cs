c8a183c9c83ed967ccdd30ec6d2a0c55a9c9a34c
OneVth <onebrotravel@gmail.com>
2024-07-23 11:07:38 +0900

    Update solution

--- Lv0/61 ~ 80/_66[홀수 vs 짝수]/Solution/Solution.cs
+++ Lv0/61 ~ 80/_66[홀수 vs 짝수]/Solution/Solution.cs
@@ -8,13 +8,26 @@ namespace Solution
 {
     internal class Solution
     {
-        public int solution(int[] num_list)
+        public int solution1(int[] num_list)
         {
 
             int oddSum = num_list.Where((w, index) => index % 2 == 0).Sum();
             int evenSum = num_list.Where((w, index) => index % 2 == 1).Sum();
 
             return oddSum >= evenSum ? oddSum : evenSum;
+        }
+
+        public int solution2(int[] num_list)
+        {
+            int even = 0;
+            for (int i = 0; i < num_list.Length; i += 2)
+                even += num_list[i];
+
+            int odd = 0;
+            for (int i = 1; i < num_list.Length; i += 2)
+                odd += num_list[i];
+
+            return Math.Max(even, odd);
         }
     }
 }
