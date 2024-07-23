6668da9b2d97c3f0c376c20293b2041851a9e9f0
OneVth <onebrotravel@gmail.com>
2024-07-23 11:12:46 +0900

    Upload solution

---
+++ Lv0/61 ~ 80/_69[n보다 커질 때까지 더하기]/Solution/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(int[] numbers, int n)
+        {
+            int answer = 0;
+            int i = 0;
+
+            while (answer <= n)
+            {
+                answer += numbers[i];
+                i++;
+            }
+
+            return answer;
+        }
+    }
+}
