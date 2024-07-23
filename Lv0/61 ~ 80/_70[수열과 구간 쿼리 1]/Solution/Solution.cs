cc65b21fcc68be1093ccc6a78928a486e37de3a2
OneVth <onebrotravel@gmail.com>
2024-07-23 11:14:56 +0900

    Upload solution

---
+++ Lv0/61 ~ 80/_70[수열과 구간 쿼리 1]/Solution/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int[] solution(int[] arr, int[,] queries)
+        {
+
+            for (int i = 0; i < queries.GetLength(0); i++)
+            {
+                int s = queries[i, 0];
+                int e = queries[i, 1];
+
+                for (int j = s; j <= e; j++) arr[j]++;
+            }
+
+            return arr;
+        }
+    }
+}
