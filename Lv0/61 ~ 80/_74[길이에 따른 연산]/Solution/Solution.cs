bc855b868e0f2ee6ecc1e32d62041c284cdabbe9
OneVth <onebrotravel@gmail.com>
2024-07-24 12:17:28 +0900

    Upload solution

---
+++ Lv0/61 ~ 80/_74[길이에 따른 연산]/Solution/Solution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(int[] num_list)
+        {
+            int answer = 1;
+
+            if (num_list.Length >= 11) return num_list.Sum();
+            else
+            {
+                foreach (int i in num_list)
+                {
+                    answer *= i;
+                }
+            }
+            return answer;
+        }
+    }
+}
