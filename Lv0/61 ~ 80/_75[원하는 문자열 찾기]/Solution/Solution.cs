c17791d137229de61b9b60d018b9e5903d779a9e
OneVth <onebrotravel@gmail.com>
2024-07-24 12:21:30 +0900

    Upload solution

---
+++ Lv0/61 ~ 80/_75[원하는 문자열 찾기]/Solution/Solution.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(string myString, string pat)
+        {
+
+            if (pat.Length > myString.Length) return 0;
+
+            return myString.ToUpper().Contains(pat.ToUpper()) ? 1 : 0;
+        }
+    }
+}
