71753cf11905cb702ebc6e917c447c8fd3a41df9
OneVth <onebrotravel@gmail.com>
2024-07-24 12:02:23 +0900

    Upload solution

---
+++ Lv0/61 ~ 80/_71[조건에 맞게 수열 변환하기 1]/Solution/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int[] solution(int[] arr)
+        {
+            List<int> list = new List<int>();
+
+            foreach (int i in arr)
+            {
+                if (i >= 50 && i % 2 == 0) list.Add(i / 2);
+                else if (i < 50 && i % 2 == 1) list.Add(i * 2);
+                else list.Add(i);
+            }
+
+            return list.ToArray();
+        }
+    }
+}
