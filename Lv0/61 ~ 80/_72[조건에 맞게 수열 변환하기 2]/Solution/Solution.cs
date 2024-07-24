26d305b4266675328cd0b430a64dd006b6738371
OneVth <onebrotravel@gmail.com>
2024-07-24 12:04:57 +0900

    Upload solution

---
+++ Lv0/61 ~ 80/_72[조건에 맞게 수열 변환하기 2]/Solution/Solution.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(int[] arr)
+        {
+            int answer = 0;
+            bool isEqual = false;
+
+            int[] curArray = new int[arr.Length];
+            int[] temp = (int[])arr.Clone();
+
+            while (!isEqual)
+            {
+                List<int> list = new List<int>();
+
+                foreach (int i in temp)
+                {
+                    if (i >= 50 && i % 2 == 0) list.Add(i / 2);
+                    else if (i < 50 && i % 2 == 1) list.Add(i * 2 + 1);
+                    else list.Add(i);
+                }
+
+                curArray = list.ToArray();
+
+                isEqual = true; // Assume arrays are equal initially
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (curArray[i] != temp[i])
+                    {
+                        isEqual = false;
+                        break;
+                    }
+                }
+
+                if (!isEqual)
+                {
+                    temp = (int[])curArray.Clone();
+                    answer++;
+                }
+            }
+
+            return answer;
+        }
+    }
+}
