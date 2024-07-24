cdc5fde98a88966cccb8a674b030c7f570e0f419
OneVth <onebrotravel@gmail.com>
2024-07-24 12:20:49 +0900

    Update solution

--- Lv0/61 ~ 80/_74[길이에 따른 연산]/Solution/Solution.cs
+++ Lv0/61 ~ 80/_74[길이에 따른 연산]/Solution/Solution.cs
@@ -8,7 +8,7 @@ namespace Solution
 {
     internal class Solution
     {
-        public int solution(int[] num_list)
+        public int solution1(int[] num_list)
         {
             int answer = 1;
 
@@ -20,6 +20,13 @@ namespace Solution
                     answer *= i;
                 }
             }
+            return answer;
+        }
+
+        public int solution2(int[] num_list)
+        {
+            int answer = num_list.Length <= 10 ? num_list.Aggregate((a, b) => a * b) : num_list.Sum();
+
             return answer;
         }
     }
