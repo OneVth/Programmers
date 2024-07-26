d6ce5ab6ce33b91421e280be854114eaca285350
OneVth <onebrotravel@gmail.com>
2024-07-26 11:32:21 +0900

    Upload solution

---
+++ Lv0/61 ~ 80/_77[소문자로 바꾸기]/Solution/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public string solution(string myString)
+        {
+            return myString.ToLower();
+        }
+    }
+}
