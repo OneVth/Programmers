89e3d414ba8655f35855e8ccb760a63d8a8407de
OneVth <onebrotravel@gmail.com>
2024-07-26 11:35:10 +0900

    Upload solution

---
+++ Lv0/61 ~ 80/_79[A 강조하기]/Solution/Solution.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public string solution(string myString)
+        {
+            string answer = "";
+            answer = myString.ToLower();
+            answer = answer.Replace('a', 'A');
+            return answer;
+        }
+    }
+}
