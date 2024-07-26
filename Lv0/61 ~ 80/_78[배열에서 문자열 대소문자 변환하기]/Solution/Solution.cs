3d51456103a841510ff1d4381b2d4505390f141e
OneVth <onebrotravel@gmail.com>
2024-07-26 11:32:56 +0900

    Upload solution

---
+++ Lv0/61 ~ 80/_78[배열에서 문자열 대소문자 변환하기]/Solution/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public string[] solution(string[] strArr)
+        {
+            string[] answer = new string[strArr.Length];
+
+            for (int i = 0; i < strArr.Length; i++)
+            {
+                if (i % 2 == 0) answer[i] = strArr[i].ToLower();
+                else answer[i] = strArr[i].ToUpper();
+            }
+
+            return answer;
+        }
+    }
+}
