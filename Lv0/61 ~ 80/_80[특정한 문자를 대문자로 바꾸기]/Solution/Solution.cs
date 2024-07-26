241963cf3615f6f1dfed13e052daca02111b0020
OneVth <onebrotravel@gmail.com>
2024-07-26 11:36:36 +0900

    Upload solution

---
+++ Lv0/61 ~ 80/_80[특정한 문자를 대문자로 바꾸기]/Solution/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public string solution(string my_string, string alp)
+        {
+            return my_string.Replace(alp, alp.ToUpper());
+        }
+    }
+}
