d7002e68837c65b8f7b40ffcc29c7138e03ce68a
OneVth <onebrotravel@gmail.com>
2024-07-27 17:10:59 +0900

    Upload solution

---
+++ Lv0/81 ~ 100/_83[ad 제거하기]/Solution/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public string[] solution(string[] strArr)
+        {
+            return strArr.Where(w => w.IndexOf("ad") == -1).ToArray();
+        }
+    }
+}
