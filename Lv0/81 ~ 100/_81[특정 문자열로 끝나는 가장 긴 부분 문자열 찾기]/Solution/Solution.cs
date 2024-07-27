49beecd74f4df669de1fe71a97427fcd53de1b2e
OneVth <onebrotravel@gmail.com>
2024-07-27 16:55:28 +0900

    Update solution

--- Lv0/81 ~ 100/_81[특정 문자열로 끝나는 가장 긴 부분 문자열 찾기]/Solution/Solution.cs
+++ Lv0/81 ~ 100/_81[특정 문자열로 끝나는 가장 긴 부분 문자열 찾기]/Solution/Solution.cs
@@ -8,13 +8,18 @@ namespace Solution
 {
     internal class Solution
     {
-        public string solution(string myString, string pat)
+        public string solution1(string myString, string pat)
         {
             int length = myString.LastIndexOf(pat) + pat.Length;
             char[] answer = new char[length];
 
             myString.CopyTo(0, answer, 0, length);
             return new string(answer);
+        }
+
+        public string solution2(string myString, string pat)
+        {
+            return myString.Substring(0, myString.LastIndexOf(pat) + pat.Length);
         }
     }
 }
