5e17580875ddb41af3d83033ffe8fdf3fa9d3b3f
OneVth <onebrotravel@gmail.com>
2024-07-27 16:52:54 +0900

    Upload solution

---
+++ Lv0/81 ~ 100/_81[특정 문자열로 끝나는 가장 긴 부분 문자열 찾기]/Solution/Solution.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public string solution(string myString, string pat)
+        {
+            int length = myString.LastIndexOf(pat) + pat.Length;
+            char[] answer = new char[length];
+
+            myString.CopyTo(0, answer, 0, length);
+            return new string(answer);
+        }
+    }
+}
