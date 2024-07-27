9ce92461e6323a8f48e616529057c70eaf1acea5
OneVth <onebrotravel@gmail.com>
2024-07-27 17:14:00 +0900

    Upload solution

---
+++ Lv0/81 ~ 100/_84[공백으로 구분하기 1]/Solution/Solution.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public string[] solution(string my_string)
+        {
+            string[] answer = new string[] { };
+            answer = my_string.Split(' ');
+            return answer;
+        }
+    }
+}
