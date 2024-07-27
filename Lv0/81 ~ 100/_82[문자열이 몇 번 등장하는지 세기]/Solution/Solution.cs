60572045869dd4b953616901bd8949cd08dc6e00
OneVth <onebrotravel@gmail.com>
2024-07-27 17:02:15 +0900

    Upload solution

---
+++ Lv0/81 ~ 100/_82[문자열이 몇 번 등장하는지 세기]/Solution/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(string myString, string pat)
+        {
+            int result = 0;
+            int count = 0;
+
+            result = myString.IndexOf(pat, 0, myString.Length - 1);
+
+            while (result != -1)
+            {
+                count++;
+                result = myString.IndexOf(pat, result + 1, myString.Length - result - 1);
+            }
+
+            return count;
+        }
+    }
+}
