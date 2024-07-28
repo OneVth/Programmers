844feb332cc78b4e695b8a75eefbf328a25f4157
OneVth <onebrotravel@gmail.com>
2024-07-28 12:16:16 +0900

    Upload solution

---
+++ Lv0/81 ~ 100/_87[문자열 잘라서 정렬하기]/Solution/Soution.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Soution
+    {
+        public string[] solution(string myString)
+        {
+            string[] answer = myString.Split("x");
+            answer = answer.Where(w => !string.IsNullOrWhiteSpace(w)).ToArray();
+            Array.Sort(answer);
+            return answer;
+        }
+    }
+}
