ad7f039c606d5374f03d416cf7cf698b9727c4b4
OneVth <onebrotravel@gmail.com>
2024-07-28 12:25:42 +0900

    Upload solution

---
+++ Lv0/81 ~ 100/_89[문자열 바꿔서 찾기]/Solution/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(string myString, string pat)
+        {
+            return new string(myString.Select(w => w == 'A' ? 'B' : 'A').ToArray()).Contains(pat) ? 1 : 0;
+        }
+    }
+}
