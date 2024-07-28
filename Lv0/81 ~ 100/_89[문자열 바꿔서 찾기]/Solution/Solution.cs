52daaa85db7c2cff5ba6a6bd7fcfce5115eef494
OneVth <onebrotravel@gmail.com>
2024-07-28 12:28:08 +0900

    Update solution

--- Lv0/81 ~ 100/_89[문자열 바꿔서 찾기]/Solution/Solution.cs
+++ Lv0/81 ~ 100/_89[문자열 바꿔서 찾기]/Solution/Solution.cs
@@ -8,9 +8,31 @@ namespace Solution
 {
     internal class Solution
     {
-        public int solution(string myString, string pat)
+        public int solution1(string myString, string pat)
         {
             return new string(myString.Select(w => w == 'A' ? 'B' : 'A').ToArray()).Contains(pat) ? 1 : 0;
+        }
+
+        public int solution2(string myString, string pat)
+        {
+            myString = myString.Replace("A", "b")
+                .Replace("B", "a");
+
+            return myString.Contains(pat.ToLower()) ? 1 : 0;
+        }
+        public int solution3(string myString, string pat)
+        {
+            List<char> list = new List<char>();
+
+            foreach (char c in myString)
+            {
+                if (c == 'A') list.Add('B');
+                else list.Add('A');
+            }
+
+
+            string temp = new string(list.ToArray());
+            return temp.Contains(pat) ? 1 : 0;
         }
     }
 }
