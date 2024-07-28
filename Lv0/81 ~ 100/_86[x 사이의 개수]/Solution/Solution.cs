491ada42e383d04819267e118f15d83ece7e0789
OneVth <onebrotravel@gmail.com>
2024-07-28 11:37:15 +0900

    Upload solution

---
+++ Lv0/81 ~ 100/_86[x 사이의 개수]/Solution/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int[] solution(string myString)
+        {
+            List<int> list = new List<int>();
+
+            string[] temp = myString.Split("x");
+
+            foreach (string s in temp)
+            {
+                list.Add(s.Length);
+            }
+
+            return list.ToArray();
+        }
+    }
+}
