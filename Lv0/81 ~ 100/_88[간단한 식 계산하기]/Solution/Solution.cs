b3f320c9faf8965584d0da37752943eb0d55be5c
OneVth <onebrotravel@gmail.com>
2024-07-28 12:19:58 +0900

    Upload solution

---
+++ Lv0/81 ~ 100/_88[간단한 식 계산하기]/Solution/Solution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(string binomial)
+        {
+            int answer = 0;
+            string[] splited = binomial.Split(" ");
+            char[] op = splited[1].ToCharArray();
+            switch ((int)op[0]) // check op
+            {
+                case 42: // op is "*"
+                    answer = int.Parse(splited[0]) * int.Parse(splited[2]);
+                    break;
+                case 43: // op is "+"
+                    answer = int.Parse(splited[0]) + int.Parse(splited[2]);
+                    break;
+                case 45: // op is "-"
+                    answer = int.Parse(splited[0]) - int.Parse(splited[2]);
+                    break;
+            }
+            return answer;
+        }
+}
