01b4790f8b2c11b499ac7035b61905c4c7d63211
OneVth <onebrotravel@gmail.com>
2024-07-29 17:42:39 +0900

    Upload solution

---
+++ Lv0/81 ~ 100/_95[무작위로 K개의 수 뽑기]/Solution/Solution.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int[] solution(int[] arr, int k)
+        {
+            List<int> list = new List<int>();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (list.Count == k) return list.ToArray();
+
+                if (list.Count == 0)
+                    list.Add(arr[i]);
+                else
+                {
+                    if (!list.Contains(arr[i])) list.Add(arr[i]);
+                }
+            }
+
+            int count = list.Count;
+
+            if (count < k)
+            {
+                for (int i = 0; i < k - count; i++)
+                {
+                    list.Add(-1);
+                }
+            }
+
+            return list.ToArray();
+        }
+    }
+}
