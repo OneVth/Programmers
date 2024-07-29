21c461dc2938dbd9b590d555d0064900fbc271f3
OneVth <onebrotravel@gmail.com>
2024-07-29 17:28:00 +0900

    Upload solution

---
+++ Lv0/81 ~ 100/_91[세 개의 구분자]/Solution/Solution.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public string[] solution(string myStr)
+        {
+            string[] answer = new string[] { };
+            char[] delimeters = { 'a', 'b', 'c' };
+
+            answer = myStr.Split(delimeters).Where(w => !string.IsNullOrWhiteSpace(w)).ToArray();
+
+            return answer.Length == 0 || answer == null ? new string[] { "EMPTY" } : answer;
+        }
+    }
+}
