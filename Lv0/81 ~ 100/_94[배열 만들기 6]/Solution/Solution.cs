ae0ce8783b1e70b5411533cda2baa6d5f3a05d31
OneVth <onebrotravel@gmail.com>
2024-07-29 17:37:23 +0900

    Upload solution

---
+++ Lv0/81 ~ 100/_94[배열 만들기 6]/Solution/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int[] solution(int[] arr)
+        {
+            int[] answer = new int[] { };
+            List<int> stk = new List<int>();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (stk.Count == 0) stk.Add(arr[i]);
+                else
+                {
+                    if (stk[stk.Count - 1] == arr[i]) stk.RemoveAt(stk.Count - 1);
+                    else stk.Add(arr[i]);
+                }
+            }
+            return stk.Count == 0 ? new int[1] { -1 } : stk.ToArray();
+        }
+    }
+}
