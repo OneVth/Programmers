2054dc9ecfd44e3f9213054b415284d13ab97d60
OneVth <onebrotravel@gmail.com>
2024-07-29 17:35:19 +0900

    Upload solution

---
+++ Lv0/81 ~ 100/_93[빈 배열에 추가, 삭제하기]/Solution/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int[] solution(int[] arr, bool[] flag)
+        {
+            List<int> list = new List<int>();
+
+            for (int i = 0; i < flag.Length; i++)
+            {
+                if (flag[i]) list.AddRange(Enumerable.Repeat(arr[i], arr[i] * 2));
+                else list.RemoveRange(list.Count - arr[i], arr[i]);
+            }
+
+            return list.ToArray();
+        }
+    }
+}
