0115aa9509ab1a4fa71dcb0327f5bd3541dec6a3
OneVth <onebrotravel@gmail.com>
2024-07-29 17:32:02 +0900

    Upload solution

---
+++ Lv0/81 ~ 100/_92[배열의 원소만큼 추가하기]/Solution/Solution.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int[] solution(int[] arr)
+        {
+            List<int> list = new List<int>();
+            foreach (int i in arr)
+            {
+                for (int j = 0; j < i; j++) list.Add(i);
+            }
+            return list.ToArray();
+        }
+    }
+}
