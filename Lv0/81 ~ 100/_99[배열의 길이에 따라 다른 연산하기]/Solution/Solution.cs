5c276bc5369df8ca0e9abf9b277b96e72aeb5c8d
OneVth <onebrotravel@gmail.com>
2024-07-30 19:04:40 +0900

    Upload solution

---
+++ Lv0/81 ~ 100/_99[배열의 길이에 따라 다른 연산하기]/Solution/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int[] solution(int[] arr, int n)
+        {
+
+            if (arr.Length % 2 == 0)
+            {
+                for (int i = 1; i < arr.Length; i += 2)
+                    arr[i] += n;
+            }
+            else
+            {
+                for (int i = 0; i < arr.Length; i += 2)
+                    arr[i] += n;
+            }
+
+            return arr;
+        }
+    }
+}
