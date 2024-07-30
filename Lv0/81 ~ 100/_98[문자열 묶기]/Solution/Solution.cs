9bcde830bc986d44f2d7b1ff83f7a6da7c6ffd5f
OneVth <onebrotravel@gmail.com>
2024-07-30 18:59:23 +0900

    Upload solution

---
+++ Lv0/81 ~ 100/_98[문자열 묶기]/Solution/Solution.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(string[] strArr)
+        {
+            int answer = 0;
+            Dictionary<int, List<string>> lengthGroups = new Dictionary<int, List<string>>();
+
+            foreach (string s in strArr)
+            {
+                int length = s.Length;
+                if (!lengthGroups.ContainsKey(length))
+                {
+                    lengthGroups[length] = new List<string>();
+                }
+                lengthGroups[length].Add(s);
+            }
+
+            foreach (List<string> list_s in lengthGroups.Values)
+            {
+                if (list_s.Count > answer)
+                    answer = list_s.Count;
+            }
+
+            return answer;
+        }
+    }
+}
