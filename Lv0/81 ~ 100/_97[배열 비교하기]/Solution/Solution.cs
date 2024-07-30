5cc34c2a4dd810ac82b60c8e509f3003e75eb7ac
OneVth <onebrotravel@gmail.com>
2024-07-30 18:52:40 +0900

    Upload solution

---
+++ Lv0/81 ~ 100/_97[배열 비교하기]/Solution/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(int[] arr1, int[] arr2)
+        {
+
+            if (arr1.Length != arr2.Length)
+                return arr1.Length > arr2.Length ? 1 : -1;
+            else
+            {
+                if (arr1.Sum() != arr2.Sum())
+                    return arr1.Sum() > arr2.Sum() ? 1 : -1;
+            }
+
+            return 0;
+        }
+    }
+}
