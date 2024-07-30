dd6d79eb10629e1e352a5197ff80120b8794ee49
OneVth <onebrotravel@gmail.com>
2024-07-30 18:46:25 +0900

    Upload solution

---
+++ Lv0/81 ~ 100/_96[배열의 길이를 2의 거듭제곱으로 만들기]/Solution/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int[] solution(int[] arr)
+        {
+            int n = 1;
+
+            while (n < arr.Length)
+                n *= 2;
+
+            int[] answer = new int[n];
+
+            Array.Copy(arr, 0, answer, 0, arr.Length);
+
+            return answer;
+        }
+    }
+}
