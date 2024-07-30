ef0fd98bee1a28b91cbdbb89d36d21b5e89706da
OneVth <onebrotravel@gmail.com>
2024-07-30 19:09:43 +0900

    Upload solution

---
+++ Lv0/81 ~ 100/_100[뒤에서 5등까지]/Solution/Solution.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int[] solution(int[] num_list)
+        {
+            return num_list.OrderBy(o => o)
+                .Take(5).ToArray();
+        }
+    }
+}
