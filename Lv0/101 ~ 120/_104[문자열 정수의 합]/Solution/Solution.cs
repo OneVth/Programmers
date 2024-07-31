921d5743b65bc7df896d8342651ff1cc669bcc09
OneVth <onebrotravel@gmail.com>
2024-07-31 12:54:02 +0900

    Upload solution

---
+++ Lv0/101 ~ 120/_104[문자열 정수의 합]/Solution/Solution.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(string num_str)
+        {
+            int answer = 0;
+
+            foreach (char c in num_str)
+            {
+                answer += (int)(c - '0');
+            }
+
+            return answer;
+        }
+    }
+}
