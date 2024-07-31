35ef67a072408495aa627c89a57d82dd9d633abb
OneVth <onebrotravel@gmail.com>
2024-07-31 12:53:26 +0900

    Update solution

---
+++ Lv0/101 ~ 120/_103[정수 부분]/Solution/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(double flo)
+        {
+            return (int)flo;
+        }
+    }
+}
