3e50fe7a5235105b4ade0c1289a043b4e0270a05
OneVth <onebrotravel@gmail.com>
2024-08-01 11:40:04 +0900

    Upload solution

---
+++ Lv0/101 ~ 120/_106[0 떼기]/Solution/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public string solution(string n_str)
+        {
+            int index = 0;
+
+            for (int i = 0; i < n_str.Length; i++)
+            {
+                if (n_str[i] != '0') break;
+                index++;
+            }
+
+            char[] answer = new char[n_str.Length - index];
+
+            n_str.CopyTo(index, answer, 0, n_str.Length - index);
+            return new string(answer);
+        }
+    }
+}
