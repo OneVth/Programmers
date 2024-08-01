f47d68144f412cdc9575c71bf135efb11e0c9e6f
OneVth <onebrotravel@gmail.com>
2024-08-01 11:57:25 +0900

    Upload solution

---
+++ Lv0/101 ~ 120/_110[부분 문자열인지 확인하기]/Solution/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(string my_string, string target)
+        {
+            return my_string.Contains(target) ? 1 : 0;
+        }
+    }
+}
