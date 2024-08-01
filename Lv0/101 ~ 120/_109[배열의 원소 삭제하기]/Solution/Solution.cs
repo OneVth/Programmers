96c60ee025e788bf9aa384cf203f1cc8b1d4183e
OneVth <onebrotravel@gmail.com>
2024-08-01 11:53:01 +0900

    Upload solution

---
+++ Lv0/101 ~ 120/_109[배열의 원소 삭제하기]/Solution/Solution.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int[] solution(int[] arr, int[] delete_list)
+        {
+            foreach (int i in delete_list)
+            {
+                arr = arr.Where(w => w != i).ToArray();
+            }
+            return arr;
+        }
+    }
+}
