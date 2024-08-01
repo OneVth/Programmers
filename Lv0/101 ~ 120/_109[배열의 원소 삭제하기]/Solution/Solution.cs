483fbc6cb67fef792e14f77a4074515bd111baa5
OneVth <onebrotravel@gmail.com>
2024-08-01 11:56:05 +0900

    Update solution

--- Lv0/101 ~ 120/_109[배열의 원소 삭제하기]/Solution/Solution.cs
+++ Lv0/101 ~ 120/_109[배열의 원소 삭제하기]/Solution/Solution.cs
@@ -8,13 +8,23 @@ namespace Solution
 {
     internal class Solution
     {
-        public int[] solution(int[] arr, int[] delete_list)
+        public int[] solution1(int[] arr, int[] delete_list)
         {
             foreach (int i in delete_list)
             {
                 arr = arr.Where(w => w != i).ToArray();
             }
             return arr;
+        }
+
+        public int[] solution2(int[] arr, int[] delete_list)
+        {
+            return arr.Where(w => !delete_list.Contains(w)).ToArray();
+        }
+
+        public int[] solution3(int[] arr, int[] delete_list)
+        {
+            return arr.Except(delete_list).ToArray();
         }
     }
 }
