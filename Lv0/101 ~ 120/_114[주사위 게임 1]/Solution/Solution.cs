684957fb17a82002978e888f23b9c6891ebfdeee
OneVth <onebrotravel@gmail.com>
2024-08-02 12:29:06 +0900

    Update solution

--- Lv0/101 ~ 120/_114[주사위 게임 1]/Solution/Solution.cs
+++ Lv0/101 ~ 120/_114[주사위 게임 1]/Solution/Solution.cs
@@ -8,7 +8,7 @@ namespace Solution
 {
     internal class Solution
     {
-        public int solution(int a, int b)
+        public int solution1(int a, int b)
         {
             int answer = 0;
 
@@ -20,6 +20,20 @@ namespace Solution
                 answer = Math.Abs(a - b);
 
             return answer;
+        }
+
+        public int solution2(int a, int b)
+        {
+            int oddCount = 0;
+            if (a % 2 == 1) ++oddCount;
+            if (b % 2 == 1) ++oddCount;
+
+            if (oddCount == 2)
+                return a * a + b * b;
+            else if (oddCount == 1)
+                return 2 * (a + b);
+            else
+                return Math.Abs(a - b);
         }
     }
 }
