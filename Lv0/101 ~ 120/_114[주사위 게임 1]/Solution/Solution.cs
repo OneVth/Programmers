3a749360313f1f9688b228311b2fb46ca0cd6781
OneVth <onebrotravel@gmail.com>
2024-08-02 12:27:42 +0900

    Upload solution

---
+++ Lv0/101 ~ 120/_114[주사위 게임 1]/Solution/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(int a, int b)
+        {
+            int answer = 0;
+
+            if (a % 2 == 1 && b % 2 == 1)
+                answer = a * a + b * b;
+            else if (a % 2 == 1 || b % 2 == 1)
+                answer = 2 * (a + b);
+            else
+                answer = Math.Abs(a - b);
+
+            return answer;
+        }
+    }
+}
