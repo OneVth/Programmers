30683a856cf91cbeeddb3397c4abc425a1ed8e7b
OneVth <onebrotravel@gmail.com>
2024-08-02 12:31:43 +0900

    Update solution

--- Lv0/101 ~ 120/_115[날짜 비교하기]/Solution/Solution.cs
+++ Lv0/101 ~ 120/_115[날짜 비교하기]/Solution/Solution.cs
@@ -8,7 +8,7 @@ namespace Solution
 {
     internal class Solution
     {
-        public int solution(int[] date1, int[] date2)
+        public int solution1(int[] date1, int[] date2)
         {
 
             if (date1[0] != date2[0])
@@ -25,6 +25,14 @@ namespace Solution
                         return 0;
                 }
             }
+        }
+
+        public int solution2(int[] date1, int[] date2)
+        {
+            int d1 = int.Parse($"{date1[0]}{date1[1]}{date1[2]}");
+            int d2 = int.Parse($"{date2[0]}{date2[1]}{date2[2]}");
+
+            return d1 < d2 ? 1 : 0;
         }
     }
 }
