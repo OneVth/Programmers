f3705c7706d7526b784bfb07666e8e022883ab37
OneVth <onebrotravel@gmail.com>
2024-08-02 12:18:11 +0900

    Upload solution

---
+++ Lv0/101 ~ 120/_112[꼬리 문자열]/Solution/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public string solution(string[] str_list, string ex)
+        {
+            return string.Join("", str_list.Where(w => !w.Contains(ex)));
+        }
+    }
+}
