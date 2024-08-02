eb79b5d4390ddf5a3e4d54991385a21cc698e3c3
OneVth <onebrotravel@gmail.com>
2024-08-02 12:22:32 +0900

    Update solution

--- Lv0/101 ~ 120/_112[꼬리 문자열]/Solution/Solution.cs
+++ Lv0/101 ~ 120/_112[꼬리 문자열]/Solution/Solution.cs
@@ -8,9 +8,26 @@ namespace Solution
 {
     internal class Solution
     {
-        public string solution(string[] str_list, string ex)
+        public string solution1(string[] str_list, string ex)
         {
             return string.Join("", str_list.Where(w => !w.Contains(ex)));
+        }
+
+        public string solution2(string[] str_list, string ex)
+        {
+            return str_list.Where(w => !w.Contains(ex))
+                .Aggregate("", (current, next) => current + next);
+        }
+
+        public string solution3(string[] str_list, string ex)
+        {
+            var sb = new StringBuilder();
+            foreach (string str in str_list)
+            {
+                if (str.Contains(ex)) continue;
+                sb.Append(str);
+            }
+            return sb.ToString();
         }
     }
 }
