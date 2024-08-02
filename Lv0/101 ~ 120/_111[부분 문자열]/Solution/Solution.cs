6ea89268383d0db3aad72de35d763414d3024c5c
OneVth <onebrotravel@gmail.com>
2024-08-02 12:17:26 +0900

    Update solution

--- Lv0/101 ~ 120/_111[부분 문자열]/Solution/Solution.cs
+++ Lv0/101 ~ 120/_111[부분 문자열]/Solution/Solution.cs
@@ -8,9 +8,14 @@ namespace Solution
 {
     internal class Solution
     {
-        public int solution(string str1, string str2)
+        public int solution1(string str1, string str2)
         {
             return str2.Contains(str1) ? 1 : 0;
+        }
+
+        public int solution2(string str1, string str2)
+        {
+            return str2.IndexOf(str1) >= 0 ? 1 : 0;
         }
     }
 }
