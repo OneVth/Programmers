9273d485638957b97d0e688c65cb685f3771ec11
OneVth <onebrotravel@gmail.com>
2024-08-03 12:20:44 +0900

    Upload solution

---
+++ Lv0/101 ~ 120/_117[그림 확대]/Solution/Solution.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public string[] solution(string[] picture, int k)
+        {
+            StringBuilder sb = new StringBuilder();
+            List<string> list = new List<string>();
+
+            foreach (string s in picture)
+            {
+                for (int i = 0; i < s.Length; i++)
+                {
+                    for (int j = 0; j < k; j++)
+                    {
+                        sb.Append(s[i]);
+                    }
+                }
+                for (int j = 0; j < k; j++)
+                {
+                    list.Add(sb.ToString());
+                }
+                sb.Clear();
+            }
+
+            return list.ToArray();
+        }
+    }
+}
