8dd3ed4a30124009b19782128c72a60c022eefa0
OneVth <onebrotravel@gmail.com>
2024-08-03 12:28:56 +0900

    Update solution

--- Lv0/101 ~ 120/_120[특이한 이차원 배열 1]/Solution/Solution.cs
+++ Lv0/101 ~ 120/_120[특이한 이차원 배열 1]/Solution/Solution.cs
@@ -8,13 +8,23 @@ namespace Solution
 {
     internal class Solution
     {
-        public int[,] solution(int n)
+        public int[,] solution1(int n)
         {
             int[,] answer = new int[n, n];
 
             for (int i = 0; i < n; i++)
                 for (int j = 0; j < n; j++)
                     if (i == j) answer[i, j] = 1;
+
+            return answer;
+        }
+
+        public int[,] solution2(int n)
+        {
+            int[,] answer = new int[n, n];
+
+            for (int i = 0; i < n; i++)
+                answer[i, i] = 1;
 
             return answer;
         }
