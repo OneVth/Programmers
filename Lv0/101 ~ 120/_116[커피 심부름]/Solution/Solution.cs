eda08209b6874cd17ea426495a1b3dd54415c126
OneVth <onebrotravel@gmail.com>
2024-08-03 12:16:30 +0900

    Upload solution

---
+++ Lv0/101 ~ 120/_116[커피 심부름]/Solution/Solution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(string[] order)
+        {
+            int answer = 0;
+
+            foreach (string s in order)
+            {
+                if (s == "iceamericano" || s == "americanoice" || s == "hotamericano" || s == "americanohot" || s == "americano" || s == "anything")
+                    answer += 4500;
+                else
+                    answer += 5000;
+            }
+
+            return answer;
+        }
+    }
+}
