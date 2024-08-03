dbc7c6f6bfd01b191b95d0bd1a03643f9118438d
OneVth <onebrotravel@gmail.com>
2024-08-03 12:24:12 +0900

    Upload solution

---
+++ Lv0/101 ~ 120/_118[조건에 맞게 수열 변환하기 3]/Solution/Solution.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int[] solution(int[] arr, int k)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (k % 2 == 0) arr[i] += k;
+                else arr[i] *= k;
+            }
+
+            return arr;
+        }
+    }
+}
