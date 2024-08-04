6c9a9081da03634fed63fea8c85662ea0793a543
OneVth <onebrotravel@gmail.com>
2024-08-04 13:41:10 +0900

    Upload solution

---
+++ Lv0/121 ~ 140/_121[정수를 나선형으로 배치하기]/Solution/Solution.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int[,] solution(int n)
+        {
+            int[,] answer = new int[n, n];
+
+            int x = -1, y = 0;
+            int nDirection = 1;
+            int cnt = 0;
+
+            for (int i = n; i > 0; i--)
+            {
+                for (int j = 0; j < (2 * i - 1); j++)
+                {
+                    if (i > j)
+                        x += nDirection;
+                    else
+                        y += nDirection;
+
+                    answer[y, x] = ++cnt;
+                }
+                nDirection = -nDirection;
+            }
+
+            return answer;
+        }
+    }
+}
