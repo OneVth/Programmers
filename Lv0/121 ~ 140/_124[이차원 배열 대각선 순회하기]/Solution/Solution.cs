2ec78bb828fe1eb99f1bff938392d13cdc6e4171
OneVth <onebrotravel@gmail.com>
2024-08-04 13:44:46 +0900

    Upload solution

---
+++ Lv0/121 ~ 140/_124[이차원 배열 대각선 순회하기]/Solution/Solution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(int[,] board, int k)
+        {
+            int answer = 0;
+
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    if (i + j <= k) answer += board[i, j];
+                }
+            }
+
+            return answer;
+        }
+    }
+}
