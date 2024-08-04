2e2d34fdca56094243f9a653b99ffb8b473949cc
OneVth <onebrotravel@gmail.com>
2024-08-04 13:47:54 +0900

    Update solution

--- Lv0/121 ~ 140/_125[옹알이 (1)]/Solution/Solution.cs
+++ Lv0/121 ~ 140/_125[옹알이 (1)]/Solution/Solution.cs
@@ -8,7 +8,7 @@ namespace Solution
 {
     internal class Solution
     {
-        public int solution(string[] babbling)
+        public int solution1(string[] babbling)
         {
             int answer = 0;
 
@@ -58,6 +58,25 @@ namespace Solution
                             answer++;
                         break;
                 }
+            }
+
+            return answer;
+        }
+
+        public int solution2(string[] babbling)
+        {
+            int answer = 0;
+            string[] talkable = { "aya", "ye", "woo", "ma" };
+
+            for(int i = 0; i < babbling.Length; i++)
+            {
+                string word = babbling[i];
+                foreach(string able in talkable)
+                {
+                    word = word.Replace(able, " ");
+                }
+                if (string.IsNullOrWhiteSpace(word))
+                    answer++;
             }
 
             return answer;
