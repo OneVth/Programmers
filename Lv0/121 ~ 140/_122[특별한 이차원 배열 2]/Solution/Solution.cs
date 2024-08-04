7ca7883f370dc4e9364930f8539aa649af6d7f11
OneVth <onebrotravel@gmail.com>
2024-08-04 13:41:42 +0900

    Upload solution

---
+++ Lv0/121 ~ 140/_122[특별한 이차원 배열 2]/Solution/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(int[,] arr)
+        {
+            int size = arr.GetLength(0);
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = i; j < size; j++)
+                {
+                    if (arr[i, j] != arr[j, i])
+                        return 0;
+                }
+            }
+
+            return 1;
+        }
+    }
+}
