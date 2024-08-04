037c353a144c33e34eb708666cd9f778d5d6d13d
OneVth <onebrotravel@gmail.com>
2024-08-04 13:42:40 +0900

    Upload solution

---
+++ Lv0/121 ~ 140/_123[정사각형으로 만들기]/Solution/Solution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int[,] solution(int[,] arr)
+        {
+            int rowSize = arr.GetLength(0);
+            int colSize = arr.GetLength(1);
+            int size = (rowSize > colSize) ? rowSize : colSize;
+
+            int[,] answer = new int[size, size];
+
+            for (int i = 0; i < rowSize; i++)
+            {
+                for (int j = 0; j < colSize; j++)
+                {
+                    answer[i, j] = arr[i, j];
+                }
+            }
+
+            return answer;
+        }
+    }
+}
