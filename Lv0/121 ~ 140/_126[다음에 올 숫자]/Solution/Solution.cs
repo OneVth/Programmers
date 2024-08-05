a5fd714ab41a5019112933aac07c3e71623ef1b6
OneVth <onebrotravel@gmail.com>
2024-08-05 15:47:09 +0900

    Upload solution

---
+++ Lv0/121 ~ 140/_126[다음에 올 숫자]/Solution/Solution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(int[] common)
+        {
+            int answer = 0;
+            int d = 0;
+
+            if ((common[2] - common[1]) == (common[1] - common[0]))
+            {
+                d = common[1] - common[0];
+                answer = common.Last() + d;
+            }
+            else
+            {
+                d = common[1] / common[0];
+                answer = common.Last() * d;
+            }
+
+            return answer;
+        }
+    }
+}
