59a300be15b3f322211e2d03fd12316d5bf5ef34
OneVth <onebrotravel@gmail.com>
2024-08-05 15:56:40 +0900

    Upload solution

---
+++ Lv0/121 ~ 140/_130[잘라서 배열로 저장하기]/Solution/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public string[] solution(string my_str, int n)
+        {
+            string[] answer = new string[] { };
+            List<string> list = new List<string>();
+
+            int iterateNum = (my_str.Length + (n - 1)) / n;
+            for (int i = 0; i < iterateNum; i++)
+            {
+                list.Add(new string(my_str.Skip(i * n).Take(n).ToArray()));
+            }
+
+            return list.ToArray();
+        }
+    }
+}
