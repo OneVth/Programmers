ec2330c6b45df162811cddb7dc3a376e42850ffd
OneVth <onebrotravel@gmail.com>
2024-08-05 15:54:06 +0900

    Upload solution

---
+++ Lv0/121 ~ 140/_129[문자열 밀기]/Solution/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(string A, string B)
+        {
+            string combined = B + B;
+            if (combined.Contains(A))
+            {
+                if (A == B) return 0;
+                else
+                    return combined.IndexOf(A);
+            }
+
+            return -1;
+        }
+    }
+}
