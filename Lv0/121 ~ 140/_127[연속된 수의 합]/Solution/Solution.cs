b9ca884b72213e93900e1cb1c95c311ced363df8
OneVth <onebrotravel@gmail.com>
2024-08-05 15:48:44 +0900

    Upload solution

---
+++ Lv0/121 ~ 140/_127[연속된 수의 합]/Solution/Solution.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int[] solution(int num, int total)
+        {
+            List<int> answer = new List<int>();
+            int std = total / num; // 기준 잡기
+
+            int right = std, left = std;
+
+            answer.Add(std);
+            for(int i = 0; i < num - 1; i++)
+            {
+                if(i % 2 == 0)
+                    answer.Add(++right);
+                else
+                    answer.Add(--left);
+            }
+
+            answer.Sort();
+            return answer.ToArray();
+        }
+    }
+}
