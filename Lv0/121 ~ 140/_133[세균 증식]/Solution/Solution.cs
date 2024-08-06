1331ae3170f4922d8c5c63398be373695e18667c
OneVth <onebrotravel@gmail.com>
2024-08-06 10:05:46 +0900

    Update solution

--- Lv0/121 ~ 140/_133[세균 증식]/Solution/Solution.cs
+++ Lv0/121 ~ 140/_133[세균 증식]/Solution/Solution.cs
@@ -8,9 +8,14 @@ namespace Solution
 {
     internal class Solution
     {
-        public int solution(int n, int t)
+        public int solution1(int n, int t)
         {
             return (int)(n * Math.Pow(2, t));
+        }
+
+        public int solution2(int n, int t)
+        {
+            return n << t;
         }
     }
 }
