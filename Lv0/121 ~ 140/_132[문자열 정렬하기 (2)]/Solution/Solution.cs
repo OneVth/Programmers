de4a5a7121db9c1cbb580b7546ba3c05921ab168
OneVth <onebrotravel@gmail.com>
2024-08-06 10:02:32 +0900

    Update solution

--- Lv0/121 ~ 140/_132[문자열 정렬하기 (2)]/Solution/Solution.cs
+++ Lv0/121 ~ 140/_132[문자열 정렬하기 (2)]/Solution/Solution.cs
@@ -8,9 +8,14 @@ namespace Solution
 {
     internal class Solution
     {
-        public string solution(string my_string)
+        public string solution1(string my_string)
         {
             return new string(my_string.ToLower().OrderBy(o => o).ToArray());
+        }
+
+        public string solution2(string my_string)
+        {
+            return string.Concat(my_string.ToLower().OrderBy(c => c));
         }
     }
 }
