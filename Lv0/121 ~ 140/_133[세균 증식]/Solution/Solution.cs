14539cd8f6def05b7990be9c6a2126ee0091aa5e
OneVth <onebrotravel@gmail.com>
2024-08-06 10:05:08 +0900

    Upload solution

---
+++ Lv0/121 ~ 140/_133[세균 증식]/Solution/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(int n, int t)
+        {
+            return (int)(n * Math.Pow(2, t));
+        }
+    }
+}
