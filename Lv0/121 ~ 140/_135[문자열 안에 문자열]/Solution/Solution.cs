aea3570fbb212d38a98f1a1ad14abc41722d02b0
OneVth <onebrotravel@gmail.com>
2024-08-06 10:09:26 +0900

    Upload solution

---
+++ Lv0/121 ~ 140/_135[문자열 안에 문자열]/Solution/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(string str1, string str2)
+        {
+            return str1.Contains(str2) ? 1 : 2;
+        }
+    }
+}
