e5dec730a452afb5d8f492e9bae6ff13a5c1b2c9
OneVth <onebrotravel@gmail.com>
2024-08-06 10:07:01 +0900

    Upload solution

---
+++ Lv0/121 ~ 140/_134[제곱수 판별하기]/Solution/Solution.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(int n)
+        {
+            int sqrt = (int)Math.Sqrt(n);
+            return (sqrt * sqrt == n) ? 1 : 2;
+        }
+    }
+}
