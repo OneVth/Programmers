2847e878547cb35fd645ba37598cec41f6e96985
OneVth <onebrotravel@gmail.com>
2024-08-07 18:38:01 +0900

    Upload solution

---
+++ Lv0/121 ~ 140/_138[n의 배수 고르기]/Solution/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int[] solution(int n, int[] numlist)
+        {
+            return numlist.Where(w => w % n == 0).ToArray();
+        }
+    }
+}
