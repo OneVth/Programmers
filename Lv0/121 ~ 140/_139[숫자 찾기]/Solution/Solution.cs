12e30aca2ceec228be4ce6917b4a936f164fc70c
OneVth <onebrotravel@gmail.com>
2024-08-07 18:40:02 +0900

    Upload solution

---
+++ Lv0/121 ~ 140/_139[숫자 찾기]/Solution/Solution.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(int num, int k)
+        {
+            int tmp = num.ToString().IndexOf(k.ToString());
+            return tmp > -1 ? tmp + 1 : -1;
+        }
+    }
+}
