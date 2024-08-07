c9866501c207bcfb4c267c2d725183829ba56bf2
OneVth <onebrotravel@gmail.com>
2024-08-07 18:41:25 +0900

    Upload solution

---
+++ Lv0/121 ~ 140/_140[배열의 유사도]/Solution/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(string[] s1, string[] s2)
+        {
+            int answer = 0;
+            for (int i = 0; i < s1.Length; i++)
+            {
+                for (int j = 0; j < s2.Length; j++)
+                {
+                    if (s1[i] == s2[j]) answer++;
+                }
+            }
+            return answer;
+        }
+    }
+}
