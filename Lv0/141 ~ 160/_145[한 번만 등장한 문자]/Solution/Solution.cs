96c87f3b2f08f2d56bd2295eb16b785d979ea41c
OneVth <onebrotravel@gmail.com>
2024-08-08 17:22:24 +0900

    Upload solution

---
+++ Lv0/141 ~ 160/_145[한 번만 등장한 문자]/Solution/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public string solution(string s)
+        {
+            List<char> list = new List<char>();
+
+            foreach (char c in s)
+            {
+                if (s.IndexOf(c) == s.LastIndexOf(c))
+                    list.Add(c);
+            }
+
+            list.Sort();
+            return new string(list.ToArray());
+        }
+    }
+}
