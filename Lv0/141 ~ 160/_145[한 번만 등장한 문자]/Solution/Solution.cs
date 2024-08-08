c0c40eba202c3f944b9cac633f6525c59a9ea281
OneVth <onebrotravel@gmail.com>
2024-08-08 17:32:59 +0900

    Update solution

--- Lv0/141 ~ 160/_145[한 번만 등장한 문자]/Solution/Solution.cs
+++ Lv0/141 ~ 160/_145[한 번만 등장한 문자]/Solution/Solution.cs
@@ -8,7 +8,7 @@ namespace Solution
 {
     internal class Solution
     {
-        public string solution(string s)
+        public string solution1(string s)
         {
             List<char> list = new List<char>();
 
@@ -20,6 +20,25 @@ namespace Solution
 
             list.Sort();
             return new string(list.ToArray());
+        }
+
+        public string solution2(string s)
+        {
+            return string.Concat(s.Where(w => s.Count(c => c == w) == 1).OrderBy(o => o));
+        }
+
+        public string solution3(string s)
+        {
+            string answer = "";
+            for(char ch = 'a'; ch <= 'z'; ch++)
+            {
+                if(s.Split(ch).Length == 2)
+                {
+                    answer += ch;
+                }
+            }
+
+            return answer;
         }
     }
 }
