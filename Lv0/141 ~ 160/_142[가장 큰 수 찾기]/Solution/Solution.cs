80ea9e2cbce7a3722e0dd9e2008a1ffee4587bb5
OneVth <onebrotravel@gmail.com>
2024-08-08 17:06:41 +0900

    Update solution

--- Lv0/141 ~ 160/_142[가장 큰 수 찾기]/Solution/Solution.cs
+++ Lv0/141 ~ 160/_142[가장 큰 수 찾기]/Solution/Solution.cs
@@ -8,13 +8,18 @@ namespace Solution
 {
     internal class Solution
     {
-        public int[] solution(int[] array)
+        public int[] solution1(int[] array)
         {
             int[] answer = new int[2];
 
             answer[0] = array.Max();
             answer[1] = Array.IndexOf(array, answer[0]);
             return answer;
+        }
+
+        public int[] solution2(int[] array)
+        {
+            return new int[2] { array.Max(), Array.IndexOf(array, array.Max()) };
         }
     }
 }
