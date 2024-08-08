9b0a1c4911e6540f7faecb8ffee01288e778661d
OneVth <onebrotravel@gmail.com>
2024-08-08 17:04:34 +0900

    Upload solution

---
+++ Lv0/141 ~ 160/_142[가장 큰 수 찾기]/Solution/Solution.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int[] solution(int[] array)
+        {
+            int[] answer = new int[2];
+
+            answer[0] = array.Max();
+            answer[1] = Array.IndexOf(array, answer[0]);
+            return answer;
+        }
+    }
+}
