44067f3aace0b6f7b9d40350baa4397424f32c22
OneVth <onebrotravel@gmail.com>
2024-08-09 12:54:13 +0900

    Upload solution

---
+++ Lv0/141 ~ 160/_146[인덱스 바꾸기]/Solution/Solution.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public string solution(string my_string, int num1, int num2)
+        {
+            char[] cArray = my_string.ToCharArray();
+
+            char tmp = cArray[num1];
+            cArray[num1] = cArray[num2];
+            cArray[num2] = tmp;
+
+            return new string(cArray);
+        }
+    }
+}
