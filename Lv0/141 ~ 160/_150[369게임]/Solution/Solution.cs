2ebeb02505d35e3db8f3a84d6522e7483773f7ff
OneVth <onebrotravel@gmail.com>
2024-08-09 12:59:25 +0900

    Upload solution

---
+++ Lv0/141 ~ 160/_150[369게임]/Solution/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(int order)
+        {
+            int answer = 0;
+
+            foreach (char c in order.ToString())
+            {
+                int tmp = int.Parse(c.ToString());
+                if ((0 != tmp) && (tmp % 3 == 0))
+                    answer++;
+            }
+
+            return answer;
+        }
+    }
+}
