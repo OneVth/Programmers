9ec24b736c612ccd651a2756194ff626c55347b1
OneVth <onebrotravel@gmail.com>
2024-08-09 12:57:47 +0900

    Upload solution

---
+++ Lv0/141 ~ 160/_149[암호 해독]/Solution/Solution.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public string solution(string cipher, int code)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = code - 1; i < cipher.Length; i += code)
+            {
+                sb.Append(cipher[i]);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
