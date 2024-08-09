f165d138e323f7b4df469c1a17c65085f3f5b258
OneVth <onebrotravel@gmail.com>
2024-08-09 12:55:38 +0900

    Upload solution

---
+++ Lv0/141 ~ 160/_147[영어가 싫어요]/Solution/Solution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public long solution(string numbers)
+        {
+            string[] szArray = new string[10] 
+            { 
+                "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" 
+            };
+
+            for (int i = 0; i < szArray.Length; i++)
+            {
+                numbers = numbers.Replace(szArray[i], i.ToString());
+            }
+
+            return long.Parse(numbers);
+        }
+    }
+}
