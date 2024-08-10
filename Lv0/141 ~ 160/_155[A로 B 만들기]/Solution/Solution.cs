13028d4d70abad5d3a41bcc8837a4a41e8486016
OneVth <onebrotravel@gmail.com>
2024-08-10 20:16:51 +0900

    Upload solution

---
+++ Lv0/141 ~ 160/_155[A로 B 만들기]/Solution/Solution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(string before, string after)
+        {
+            char[] distinctedA = after.Distinct().OrderBy(o => o).ToArray();
+            char[] distinctedB = before.Distinct().OrderBy(o => o).ToArray();
+
+            if (!distinctedA.SequenceEqual(distinctedB))
+                return 0;
+
+            foreach (char c in distinctedA)
+            {
+                int countedA = after.Count(x => x == c);
+                int countedB = before.Count(x => x == c);
+                if (countedA != countedB)
+                    return 0;
+            }
+
+            return 1;
+        }
+    }
+}
