dde975a1593f61945e8c352734ddc789b2a5d604
OneVth <onebrotravel@gmail.com>
2024-08-10 20:05:24 +0900

    Upload solution

--- Lv0/141 ~ 160/_152[삼각형의 완성조건 (1)]/Solution/Solution.cs
+++ Lv0/141 ~ 160/_152[삼각형의 완성조건 (1)]/Solution/Solution.cs
@@ -8,5 +8,9 @@ namespace Solution
 {
     internal class Solution
     {
+        public int solution(int[] sides)
+        {
+            return (sides.Max() < sides.Sum() - sides.Max()) ? 1 : 2;
+        }
     }
 }
