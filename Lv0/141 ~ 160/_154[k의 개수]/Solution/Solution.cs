ec703c798eded1ab6809f77ae43206f0486cf144
OneVth <onebrotravel@gmail.com>
2024-08-10 20:09:00 +0900

    Upload solution

---
+++ Lv0/141 ~ 160/_154[k의 개수]/Solution/Solution.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(int i, int j, int k)
+        {
+            int answer = 0;
+
+            for (; i <= j; i++)
+            {
+                answer += i.ToString().Count(c => c == '0' + k);
+            }
+
+            return answer;
+        }
+    }
+}
