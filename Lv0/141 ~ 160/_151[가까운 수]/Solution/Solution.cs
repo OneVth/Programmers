43704c3bf6f044c22ae72afad21443feed0c9fc6
OneVth <onebrotravel@gmail.com>
2024-08-10 19:58:20 +0900

    Upload solution

---
+++ Lv0/141 ~ 160/_151[가까운 수]/Solution/Solution.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(int[] array, int n)
+        {
+            Array.Sort(array);
+            int minDiff = Math.Abs(array[0] - n), minIdx = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                int curIdx = i;
+                int curDiff = Math.Abs(array[i] - n);
+
+                if (curDiff < minDiff)
+                {
+                    minDiff = curDiff;
+                    minIdx = curIdx;
+                }
+            }
+
+            return array[minIdx];
+        }
+    }
+}
