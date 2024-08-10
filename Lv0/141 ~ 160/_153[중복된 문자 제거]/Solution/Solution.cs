805323823fa257751412dbd88e0fa93aa0f4943d
OneVth <onebrotravel@gmail.com>
2024-08-10 20:06:46 +0900

    Upload solution

---
+++ Lv0/141 ~ 160/_153[중복된 문자 제거]/Solution/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public string solution(string my_string)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (char c in my_string)
+            {
+                if (!sb.ToString().Contains(c))
+                    sb.Append(c);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
