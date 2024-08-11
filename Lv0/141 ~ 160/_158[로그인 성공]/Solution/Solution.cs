9d4ef35d3121de7c90056c429b6ce8228b8cc06b
OneVth <onebrotravel@gmail.com>
2024-08-11 19:58:05 +0900

    Upload solution

---
+++ Lv0/141 ~ 160/_158[로그인 성공]/Solution/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public string solution(string[] id_pw, string[,] db)
+        {
+            string answer = "fail";
+
+            for (int i = 0; i < db.GetLength(0); i++)
+            {
+                if (db[i, 0] == id_pw[0])
+                {
+                    answer = "wrong pw";
+                    if (db[i, 1] == id_pw[1])
+                        answer = "login";
+                }
+
+            }
+
+            return answer;
+        }
+    }
+}
