85397ef5fd1149925ee35a77e8da3c931ab93e7f
OneVth <onebrotravel@gmail.com>
2024-08-11 20:07:25 +0900

    Update solution

--- Lv0/141 ~ 160/_160[특이한 정렬]/Solution/Solution.cs
+++ Lv0/141 ~ 160/_160[특이한 정렬]/Solution/Solution.cs
@@ -8,9 +8,14 @@ namespace Solution
 {
     internal class Solution
     {
-        public int[] solution(int[] numlist, int n)
+        public int[] solution1(int[] numlist, int n)
         {
             return numlist.OrderByDescending(o => o).OrderBy(o => Math.Abs(o - n)).ToArray();
+        }
+
+        public int[] solution2(int[] numlist, int n)
+        {
+            return numlist.OrderBy(x => Math.Abs(n - x)).ThenByDescending(x => x).ToArray();
         }
     }
 }
