745c4b33e9132f1d80d51ecca9fb600511788820
OneVth <onebrotravel@gmail.com>
2024-08-11 19:50:22 +0900

    Upload solution

---
+++ Lv0/141 ~ 160/_156[이진수 더하기]/Solution/Solution.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public string solution(string bin1, string bin2)
+        {
+            int decBin1 = Convert.ToInt32(bin1, 2);
+            int decBin2 = Convert.ToInt32(bin2, 2);
+
+            return Convert.ToString(decBin1 + decBin2, 2);
+        }
+    }
+}
