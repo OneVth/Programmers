c7680dfd37f4da873de1c8716c11aa7d51bcb46f
OneVth <onebrotravel@gmail.com>
2024-08-11 19:53:10 +0900

    Upload solution

---
+++ Lv0/141 ~ 160/_157[치킨 쿠폰]/Solution/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(int chicken)
+        {
+            int nMax = 0;
+            int coupon = chicken;
+
+            while (coupon >= 10)
+            {
+                nMax += coupon / 10;
+                coupon = coupon / 10 + coupon % 10;
+            }
+
+            return nMax;
+        }
+    }
+}
