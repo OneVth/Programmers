3c352255e506a8a2d5823934e99825b1e09c2ad8
OneVth <onebrotravel@gmail.com>
2024-08-12 18:12:23 +0900

    Upload solution

---
+++ Lv0/161 ~ 180/_165[외계어 사전]/Solution/Solution.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(string[] spell, string[] dic)
+        {
+            string sortedSpell = String.Concat(spell.OrderBy(o => o));
+            string[] sortedDic = 
+                dic.Select(word => new string(word.OrderBy(o => o).ToArray()))
+                .ToArray();
+
+            return Array.IndexOf(sortedDic, sortedSpell) > -1 ? 1 : 2;
+        }
+    }
+}
