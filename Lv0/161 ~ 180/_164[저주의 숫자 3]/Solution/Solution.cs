b09d405109bcdea1138b6b6861f6f9418fe89ede
OneVth <onebrotravel@gmail.com>
2024-08-12 18:11:25 +0900

    Upload solution

---
+++ Lv0/161 ~ 180/_164[저주의 숫자 3]/Solution/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(int n)
+        {
+            int answer = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                ++answer;
+                while (answer.ToString().Contains('3') || (answer % 3 == 0))
+                    ++answer;
+            }
+
+            return answer;
+        }
+    }
+}
