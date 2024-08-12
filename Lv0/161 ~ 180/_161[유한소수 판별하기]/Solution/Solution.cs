ffaa18da9183deeda6f73e0ba6ec0c177f6711e3
OneVth <onebrotravel@gmail.com>
2024-08-12 17:59:23 +0900

    Upload solution

---
+++ Lv0/161 ~ 180/_161[유한소수 판별하기]/Solution/Solution.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(int a, int b)
+        {
+            int GCD = 0, denominator = 0;
+            int x = a, y = b;
+
+            // 두 수의 공약수 구하기
+            while (y != 0)
+            {
+                int temp = y;
+                y = x % y;
+                x = temp;
+            }
+
+            GCD = x;
+
+            // 분모를 최대공약수로 약분
+            denominator = b / GCD;
+
+            // 분모가 2와 5만을 소인수로 갖는지 확인
+            while (denominator % 2 == 0)
+                denominator /= 2;
+
+            while (denominator % 5 == 0)
+                denominator /= 5;
+
+            return denominator == 1 ? 1 : 2;
+        }
+    }
+}
