6e7d5ac74ecad8dfc86e99f4d84f39e22a66298e
OneVth <onebrotravel@gmail.com>
2024-08-13 14:51:50 +0900

    Update solution

--- Lv0/161 ~ 180/_168[숨어있는 숫자의 덧셈 (2)]/Solution/Solution.cs
+++ Lv0/161 ~ 180/_168[숨어있는 숫자의 덧셈 (2)]/Solution/Solution.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Solution
 {
     internal class Solution
     {
-        public int solution(string my_string)
+        public int solution1(string my_string)
         {
             int answer = 0;
             char[] alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
@@ -21,6 +22,20 @@ namespace Solution
             }
 
             return answer;
+        }
+
+        public int solution2(string my_string)
+        {
+            MatchCollection matches = Regex.Matches(my_string, @"\d+");
+
+            int totalSum = 0;
+
+            foreach (Match match in matches)
+            {
+                totalSum += int.Parse(match.Value);
+            }
+
+            return totalSum;
         }
     }
 }
