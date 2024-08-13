9e9ff98fae0fa2ad0ca098adcb35109a04272e75
OneVth <onebrotravel@gmail.com>
2024-08-13 14:54:28 +0900

    Upload solution

---
+++ Lv0/161 ~ 180/_169[다항식 더하기]/Solution/Solution.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public string solution(string polynomial)
+        {
+            string answer = "";
+            int constant = 0;
+            int coefficient = 0;
+
+            string[] array = polynomial.Split(" + ");
+
+            foreach (string s in array)
+            {
+                if (s.Contains('x'))
+                {
+                    // Handle coefficient for terms with 'x'
+                    if (s == "x")
+                        coefficient++;
+                    else
+                        coefficient += int.Parse(s.Replace("x", ""));
+                }
+                else
+                {
+                    // Handle constant terms
+                    constant += int.Parse(s);
+                }
+            }
+
+            // Building the result string
+            if (0 != coefficient)
+            {
+                answer = (coefficient == 1) ? "x" : coefficient + "x";
+            }
+
+            if (0 != constant)
+            {
+                if ("" != answer)
+                {
+                    answer += " + ";
+                }
+                answer += constant.ToString();
+            }
+
+            return answer;
+        }
+    }
+}
