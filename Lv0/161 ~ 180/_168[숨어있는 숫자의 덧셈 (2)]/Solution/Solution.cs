e5b7a253bf12f6119d28eb1e21a3eb925f16038b
OneVth <onebrotravel@gmail.com>
2024-08-13 14:42:05 +0900

    Upload solution

---
+++ Lv0/161 ~ 180/_168[숨어있는 숫자의 덧셈 (2)]/Solution/Solution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(string my_string)
+        {
+            int answer = 0;
+            char[] alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+            string[] array = my_string.Split(alpha);
+
+            foreach (string s in array)
+            {
+                if (int.TryParse(s, out int num))
+                    answer += num;
+            }
+
+            return answer;
+        }
+    }
+}
