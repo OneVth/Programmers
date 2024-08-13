25b1e51f33b7532ef1d3ad4fd32bd8f2142bc7eb
OneVth <onebrotravel@gmail.com>
2024-08-13 14:17:12 +0900

    Upload solution

---
+++ Lv0/161 ~ 180/_166[삼각형의 완성조건 (2)/Solution/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(int[] sides)
+        {
+            int answer = 0;
+            int bigger = Math.Max(sides[0], sides[1]);
+            int smaller = Math.Min(sides[0], sides[1]);
+
+            // case1 
+            // bigger가 가장 긴 변의 길이일 경우
+            answer += smaller;
+
+            // case2
+            // 나머지 변 n이 가장 긴 변의 길이일 경우
+            answer += smaller - 1;
+            return answer;
+        }
+    }
+}
