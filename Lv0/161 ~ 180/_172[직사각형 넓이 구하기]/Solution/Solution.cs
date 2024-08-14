356b5354326bc21f28653c354db07e03b834a503
OneVth <onebrotravel@gmail.com>
2024-08-14 12:06:14 +0900

    Upload solution

---
+++ Lv0/161 ~ 180/_172[직사각형 넓이 구하기]/Solution/Solution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(int[,] dots)
+        {
+            int dx = 0, dy = 0;
+
+            for (int i = 0; i < dots.GetLength(0); i++)
+            {
+                if (0 != dots[0, 0] - dots[i, 0])
+                    dx = dots[0, 0] - dots[i, 0];
+
+                if (0 != dots[0, 1] - dots[i, 1])
+                    dy = dots[0, 1] - dots[i, 1];
+            }
+            return Math.Abs(dx * dy);
+        }
+    }
+}
