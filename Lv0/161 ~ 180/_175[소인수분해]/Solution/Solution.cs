43773e55f4f0720834f8759ac98905d019d469a7
OneVth <onebrotravel@gmail.com>
2024-08-14 12:22:01 +0900

    Upload solution

---
+++ Lv0/161 ~ 180/_175[소인수분해]/Solution/Solution.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int[] solution(int n)
+        {
+            List<int> factors = new List<int>();
+
+            while (n % 2 == 0)
+            {
+                factors.Add(2);
+                n /= 2;
+            }
+
+            for (int i = 3; i <= Math.Sqrt(n); i += 2)
+            {
+                while (n % i == 0)
+                {
+                    factors.Add(i);
+                    n /= i;
+                }
+            }
+
+            if (n > 2)
+                factors.Add(n);
+
+            return factors.Distinct().ToArray();
+        }
+    }
+}
