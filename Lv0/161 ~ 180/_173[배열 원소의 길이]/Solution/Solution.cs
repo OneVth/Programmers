f9d92de0a213bcbd057a9783e865104c018cbfd8
OneVth <onebrotravel@gmail.com>
2024-08-14 12:18:49 +0900

    Upload solution

---
+++ Lv0/161 ~ 180/_173[배열 원소의 길이]/Solution/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int[] solution(string[] strlist)
+        {
+            return strlist.Select(s => s.Length).ToArray(); ;
+        }
+    }
+}
