b0d5f6b81e18f3524a05438aa2b099c203468642
OneVth <onebrotravel@gmail.com>
2024-08-14 12:19:20 +0900

    Upload solution

---
+++ Lv0/161 ~ 180/_174[컨트롤 제트]/Solution/Solution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(string s)
+        {
+            int answer = 0;
+            string[] tokens = s.Split(" ");
+
+            for (int i = 0; i < tokens.Length; i++)
+            {
+                if ("Z" == tokens[i])
+                    answer -= int.Parse(tokens[i - 1]);
+                else
+                    answer += int.Parse(tokens[i]);
+            }
+            return answer;
+        }
+    }
+}
