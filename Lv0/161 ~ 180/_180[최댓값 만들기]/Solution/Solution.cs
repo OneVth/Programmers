167aee0f4fa2d9362a97028563a97be74c171634
OneVth <onebrotravel@gmail.com>
2024-08-15 14:47:47 +0900

    Upload solution

---
+++ Lv0/161 ~ 180/_180[최댓값 만들기]/Solution/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(int[] numbers)
+        {
+            return numbers.OrderByDescending(o => o).Take(2).Aggregate((x, y) => x * y);
+        }
+    }
+}
