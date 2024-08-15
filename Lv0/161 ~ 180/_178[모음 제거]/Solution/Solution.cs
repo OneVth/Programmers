19c4b5431a4e0ecd451c81abc61836d7830c9f20
OneVth <onebrotravel@gmail.com>
2024-08-15 14:33:39 +0900

    Upload solution

---
+++ Lv0/161 ~ 180/_178[모음 제거]/Solution/Solution.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public string solution(string my_string)
+        {
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+
+            return new string(my_string.Where(w => !vowels.Contains(w)).ToArray());
+        }
+    }
+}
