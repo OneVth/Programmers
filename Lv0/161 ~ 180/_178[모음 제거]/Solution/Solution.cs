8de90b1879c0ee04e9109f350945e072d8cb55b7
OneVth <onebrotravel@gmail.com>
2024-08-15 14:43:39 +0900

    Upload solution

--- Lv0/161 ~ 180/_178[모음 제거]/Solution/Solution.cs
+++ Lv0/161 ~ 180/_178[모음 제거]/Solution/Solution.cs
@@ -2,17 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Solution
 {
     internal class Solution
     {
-        public string solution(string my_string)
+        public string solution1(string my_string)
         {
             char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
 
             return new string(my_string.Where(w => !vowels.Contains(w)).ToArray());
+        }
+
+        public string solution2(string my_string) 
+        {
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+
+            foreach (char c in vowels)
+            {
+                my_string = my_string.Replace(c.ToString(), "");
+            }
+            return my_string; 
+        }
+
+        public string solution3(string my_string)
+        {
+            return Regex.Replace(my_string, @"a|e|i|o|u", "");
         }
     }
 }
