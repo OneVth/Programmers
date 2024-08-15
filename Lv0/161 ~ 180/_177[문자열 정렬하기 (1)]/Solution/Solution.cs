04439f734f706192e64ee184887f10e185c6d962
OneVth <onebrotravel@gmail.com>
2024-08-15 14:32:41 +0900

    Update solution

--- Lv0/161 ~ 180/_177[문자열 정렬하기 (1)]/Solution/Solution.cs
+++ Lv0/161 ~ 180/_177[문자열 정렬하기 (1)]/Solution/Solution.cs
@@ -3,18 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace Solution
 {
     internal class Solution
     {
-        public int[] solution(string my_string)
+        public int[] solution1(string my_string)
         {
             return my_string
                 .Where(w => char.IsNumber(w))
                 .Select(s => int.Parse(s.ToString()))
                 .OrderBy(o => o)
                 .ToArray();
+        }
+
+        public int[] solution2(string my_string)
+        {
+            my_string = Regex.Replace(my_string, "[^0-9]", "");
+            int[] answer = my_string.Select(x => int.Parse(x.ToString())).ToArray();
+            Array.Sort(answer);
+            return answer;
         }
     }
 }
