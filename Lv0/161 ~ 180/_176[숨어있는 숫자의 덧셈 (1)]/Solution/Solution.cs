032123dfca7f931327e94707068eb0fcb9cc5a46
OneVth <onebrotravel@gmail.com>
2024-08-15 14:27:58 +0900

    Upload solution

---
+++ Lv0/161 ~ 180/_176[숨어있는 숫자의 덧셈 (1)]/Solution/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(string my_string)
+        {
+            int answer = 0;
+
+            foreach (char c in my_string)
+            {
+                if (int.TryParse(c.ToString(), out int i))
+                    answer += i;
+            }
+
+            return answer;
+        }
+    }
+}
