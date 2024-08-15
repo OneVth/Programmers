41f9318baf7613927ea38bd0bb6145704fb3d8b6
OneVth <onebrotravel@gmail.com>
2024-08-15 14:29:55 +0900

    Upload solution

---
+++ Lv0/161 ~ 180/_177[문자열 정렬하기 (1)]/Solution/Solution.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int[] solution(string my_string)
+        {
+            return my_string
+                .Where(w => char.IsNumber(w))
+                .Select(s => int.Parse(s.ToString()))
+                .OrderBy(o => o)
+                .ToArray();
+        }
+    }
+}
