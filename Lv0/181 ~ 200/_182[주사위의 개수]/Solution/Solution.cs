2ce34decec136eef1f9abc86300e9c9b1c9348b0
OneVth <onebrotravel@gmail.com>
2024-08-16 11:41:38 +0900

    Upload solution

---
+++ Lv0/181 ~ 200/_182[주사위의 개수]/Solution/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(int[] box, int n)
+        {
+            return (box[0] / n) * (box[1] / n) * (box[2] / n);
+        }
+    }
+}
