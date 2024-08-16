9aff04c40f7b96fc58ef87a929cab53dc1c08bc2
OneVth <onebrotravel@gmail.com>
2024-08-16 11:42:34 +0900

    Upload solution

---
+++ Lv0/181 ~ 200/_183[배열 회전시키기]/Solution/Solution.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int[] solution(int[] numbers, string direction)
+        {
+            List<int> list = new List<int>();
+            list.AddRange(numbers);
+
+            switch (direction)
+            {
+                case "right":
+                    list.Insert(0, list[list.Count - 1]);
+                    list.RemoveAt(list.Count - 1);
+                    break;
+                case "left":
+                    list.Add(list[0]);
+                    list.RemoveAt(0);
+                    break;
+            }
+
+            return list.ToArray();
+        }
+    }
+}
