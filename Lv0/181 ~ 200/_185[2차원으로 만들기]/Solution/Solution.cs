c570f0da72d142963851815225647404d39b2a12
OneVth <onebrotravel@gmail.com>
2024-08-16 11:56:25 +0900

    Uplaod solution

---
+++ Lv0/181 ~ 200/_185[2차원으로 만들기]/Solution/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int[,] solution(int[] num_list, int n)
+        {
+            int nRow = num_list.Length / n;
+            int nCol = n;
+            int[,] answer = new int[nRow, nCol];
+
+            for (int i = 0; i < nRow; i++)
+            {
+                for (int j = 0; j < nCol; j++)
+                {
+                    answer[i, j] = num_list[i * n + j];
+                }
+            }
+
+            return answer;
+        }
+    }
+}
