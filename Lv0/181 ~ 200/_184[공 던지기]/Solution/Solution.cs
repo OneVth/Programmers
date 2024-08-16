b7d86a9a1baeb4440ee9bdf9ed1c0f29729bdf0f
OneVth <onebrotravel@gmail.com>
2024-08-16 11:47:01 +0900

    Upload solution

---
+++ Lv0/181 ~ 200/_184[공 던지기]/Solution/Solution.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(int[] numbers, int k)
+        {
+            int answer = 1;
+
+            for (int i = 1; i < k; i++)
+                answer = (answer + 2) > numbers.Length ? answer + 2 - numbers.Length : answer + 2;
+
+            return answer;
+        }
+    }
+}
