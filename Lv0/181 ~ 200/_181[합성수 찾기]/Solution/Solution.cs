20c78ccf3f8637a8aa0654efb90e73d7c73ec61a
OneVth <onebrotravel@gmail.com>
2024-08-16 11:37:12 +0900

    Upload solution

---
+++ Lv0/181 ~ 200/_181[합성수 찾기]/Solution/Solution.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(int n)
+        {
+            int answer = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                int nDivisor = 0;
+                for (int j = 1; j <= i; j++)
+                {
+                    if (i % j == 0)
+                        nDivisor++;
+
+                    if (nDivisor >= 3)
+                    {
+                        Console.WriteLine(i);
+                        answer++;
+                        break;
+                    }
+                }
+            }
+
+            return answer;
+        }
+    }
+}
