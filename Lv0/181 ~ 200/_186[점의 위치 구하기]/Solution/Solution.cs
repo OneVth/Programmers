f0fd9f67a1d306106ca08cd5e7a449168125f79b
OneVth <onebrotravel@gmail.com>
2024-08-17 12:30:47 +0900

    Upload solution

---
+++ Lv0/181 ~ 200/_186[점의 위치 구하기]/Solution/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(int[] dot)
+        {
+            int answer = 0;
+
+            if (0 < dot[0])
+            {
+                answer = 0 < dot[1] ? 1 : 4;
+            }
+            else
+            {
+                answer = 0 > dot[1] ? 3 : 2;
+            }
+
+            return answer;
+        }
+    }
+}
