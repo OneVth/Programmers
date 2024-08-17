3e9decf3ad966a7f0d19bd1ab9ebb8370b1f6e34
OneVth <onebrotravel@gmail.com>
2024-08-17 12:33:35 +0900

    Update solution

--- Lv0/181 ~ 200/_186[점의 위치 구하기]/Solution/Solution.cs
+++ Lv0/181 ~ 200/_186[점의 위치 구하기]/Solution/Solution.cs
@@ -8,7 +8,7 @@ namespace Solution
 {
     internal class Solution
     {
-        public int solution(int[] dot)
+        public int solution1(int[] dot)
         {
             int answer = 0;
 
@@ -22,6 +22,23 @@ namespace Solution
             }
 
             return answer;
+        }
+
+        public int solution2(int[] dot)
+        {
+            int x = dot[0];
+            int y = dot[1];
+
+            if (0 < x && 0 < y)
+                return 1; // First Quadrant
+            else if (0 > x && 0 < y)
+                return 2; // Second Quadrant
+            else if (0 > x && 0 > y)
+                return 3; // Third Quadrant
+            else if (0 < x && 0 > y)
+                return 4; // Fourth Quadrant
+
+            return 0;
         }
     }
 }
