5b35ab3a0bcd8521c7f45c3ca9380779ba6b7b3a
OneVth <onebrotravel@gmail.com>
2024-08-17 13:16:13 +0900

    Upload solution

---
+++ Lv0/181 ~ 200/_190[개미 군단]/Solution/Solution.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(int hp)
+        {
+            int answer = 0;
+            answer += hp / 5;
+            answer += hp % 5 / 3;
+            answer += hp % 5 % 3;
+            return answer;
+        }
+    }
+}
