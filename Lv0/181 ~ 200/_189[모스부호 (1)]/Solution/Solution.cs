b5e4212be71d6cf9500600b8f2881cfa90609136
OneVth <onebrotravel@gmail.com>
2024-08-17 13:10:53 +0900

    Upload solution

---
+++ Lv0/181 ~ 200/_189[모스부호 (1)]/Solution/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public string solution(string letter)
+        {
+            // a ~ z
+            string[] morse =
+            {
+            ".-", "-...", "-.-.", "-..", ".", "..-.",
+            "--.", "....", "..", ".---", "-.-", ".-..",
+            "--", "-.", "---", ".--.", "--.-", ".-.",
+            "...", "-", "..-", "...-", ".--", "-..-",
+            "-.--", "--.."
+        };
+
+            StringBuilder sb = new StringBuilder();
+            string[] token = letter.Split(' ');
+
+            for (int i = 0; i < token.Length; i++)
+            {
+                sb.Append((char)(Array.IndexOf(morse, token[i]) + 'a'));
+            }
+            return sb.ToString();
+        }
+    }
+}
