131a1dd5f62b5b6f5d3ac5e9fef49f3c1912071e
OneVth <onebrotravel@gmail.com>
2024-08-17 13:05:55 +0900

    Upload solution

---
+++ Lv0/181 ~ 200/_188[가위 바위 보]/Solution/Solution.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public string solution(string rsp)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (char c in rsp)
+            {
+                switch (c)
+                {
+                    case '0':
+                        sb.Append('5');
+                        break;
+                    case '2':
+                        sb.Append('0');
+                        break;
+                    case '5':
+                        sb.Append('2');
+                        break;
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
