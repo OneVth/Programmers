c3487abda95a54282ccedaab85d141b12ac20080
OneVth <onebrotravel@gmail.com>
2024-08-17 13:17:21 +0900

    Update solution

--- Lv0/181 ~ 200/_190[개미 군단]/Solution/Solution.cs
+++ Lv0/181 ~ 200/_190[개미 군단]/Solution/Solution.cs
@@ -8,12 +8,25 @@ namespace Solution
 {
     internal class Solution
     {
-        public int solution(int hp)
+        public int solution1(int hp)
         {
             int answer = 0;
             answer += hp / 5;
             answer += hp % 5 / 3;
             answer += hp % 5 % 3;
+            return answer;
+        }
+
+        public int solution2(int hp)
+        {
+            int answer = 0;
+            answer += hp / 5;
+            hp = hp % 5;
+
+            answer += hp / 3;
+            hp = hp % 3;
+
+            answer += hp;
             return answer;
         }
     }
