66416193055fa7a09130698f802a4275f1b4258c
OneVth <onebrotravel@gmail.com>
2024-08-17 12:35:13 +0900

    Upload solution

---
+++ Lv0/181 ~ 200/_187[구슬을 나누는 경우의 수]/Solution/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(int balls, int share)
+        {
+            double numerator = 1, denominator = 1;
+
+            // n! / (n - m)! * m!
+            for (int i = 0; i < balls - share; i++)
+                numerator *= balls - i;
+
+            for (int i = 0; i < balls - share; i++)
+                denominator *= balls - share - i;
+            return (int)(numerator / denominator);
+        }
+    }
+}
