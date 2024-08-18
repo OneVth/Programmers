57648bd219fb7da33e43205f605dad87b602af5f
OneVth <onebrotravel@gmail.com>
2024-08-18 14:22:12 +0900

    Update solution

--- Lv0/181 ~ 200/_194[배열 자르기]/Solution/Solution.cs
+++ Lv0/181 ~ 200/_194[배열 자르기]/Solution/Solution.cs
@@ -8,12 +8,17 @@ namespace Solution
 {
     internal class Solution
     {
-        public int[] solution(int[] numbers, int num1, int num2)
+        public int[] solution1(int[] numbers, int num1, int num2)
         {
             int length = num2 - num1 + 1;
             int[] answer = new int[length];
             Array.Copy(numbers, num1, answer, 0, length);
             return answer;
+        }
+
+        public int[] solution2(int[] numbers, int num1, int num2)
+        {
+            return numbers.Skip(num1).Take(num2 - num1 + 1).ToArray();
         }
     }
 }
