813fecb771c08f07418641a07fc7bf1d3e05d04a
OneVth <onebrotravel@gmail.com>
2024-08-18 14:10:38 +0900

    Upload solution

---
+++ Lv0/181 ~ 200/_192[진료순서 정하기]/Solution/Solution.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int[] solution(int[] emergency)
+        {
+            List<int> list = new List<int>();
+            var temp = emergency.OrderByDescending(o => o).ToArray();
+
+            for (int i = 0; i < temp.Length; i++)
+                list.Add(Array.IndexOf(temp, emergency[i]) + 1);
+
+            return list.ToArray();
+        }
+    }
+}
