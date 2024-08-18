3260b3e184d5fd17f8f79a305bbadc314d44012f
OneVth <onebrotravel@gmail.com>
2024-08-19 07:41:29 +0900

    Upload solution

---
+++ Lv0/181 ~ 200/_199[문자 반복 출력하기]/Solution/Solution.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public string solution(string my_string, int n)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (char c in my_string)
+                sb.Append(c, n);
+
+            return sb.ToString();
+        }
+    }
+}
