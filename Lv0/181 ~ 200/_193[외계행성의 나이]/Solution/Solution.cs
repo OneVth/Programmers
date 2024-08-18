6fb9f6ea0bee106a2aaa2577dacb4f6646cae278
OneVth <onebrotravel@gmail.com>
2024-08-18 14:14:30 +0900

    Upload solution

---
+++ Lv0/181 ~ 200/_193[외계행성의 나이]/Solution/Solution.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public string solution(int age)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (char c in age.ToString())
+                sb.Append((char)(int.Parse(c.ToString()) + 'a'));
+
+            return sb.ToString();
+        }
+    }
+}
