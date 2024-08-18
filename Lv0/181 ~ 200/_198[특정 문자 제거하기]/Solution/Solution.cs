0037b82c77cf64651891160f0ca85eeba82cbe2f
OneVth <onebrotravel@gmail.com>
2024-08-19 07:39:55 +0900

    Upload solution

---
+++ Lv0/181 ~ 200/_198[특정 문자 제거하기]/Solution/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public string solution(string my_string, string letter)
+        {
+            return my_string.Replace(letter, "");
+        }
+    }
+}
