94520f0b289b5cf46358d37dce764d6363539020
OneVth <onebrotravel@gmail.com>
2024-08-19 07:38:44 +0900

    Upload solution

---
+++ Lv0/181 ~ 200/_197[각도기]/Solution/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(int angle)
+        {
+            int answer = 0;
+
+            if (90 > angle)
+                answer = 1;
+            else if (90 == angle)
+                answer = 2;
+            else if (180 > angle)
+                answer = 3;
+            else if (180 == angle)
+                answer = 4;
+
+            return answer;
+        }
+    }
+}
