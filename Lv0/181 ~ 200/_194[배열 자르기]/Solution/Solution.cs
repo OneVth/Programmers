bc4402cebc945f66524b09f26931480c026c1e7e
OneVth <onebrotravel@gmail.com>
2024-08-18 14:20:51 +0900

    Upload solution

---
+++ Lv0/181 ~ 200/_194[배열 자르기]/Solution/Solution.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int[] solution(int[] numbers, int num1, int num2)
+        {
+            int length = num2 - num1 + 1;
+            int[] answer = new int[length];
+            Array.Copy(numbers, num1, answer, 0, length);
+            return answer;
+        }
+    }
+}
