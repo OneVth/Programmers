55dbbf0ab00b37139c11d8baa6a0d06221131ad2
OneVth <onebrotravel@gmail.com>
2024-08-19 07:42:41 +0900

    Upload solution

---
+++ Lv0/181 ~ 200/_200[짝수 홀수 개수]/Solution/Solution.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int[] solution(int[] num_list)
+        {
+            int[] oddOrEven = new int[2];
+
+            oddOrEven[0] = num_list.Count(x => x % 2 == 0);
+            oddOrEven[1] = num_list.Count(x => x % 2 == 1);
+
+            return oddOrEven;
+        }
+    }
+}
