bcdc51d45ea2a6da7bd25218016a2990efddf39f
OneVth <onebrotravel@gmail.com>
2024-08-20 11:52:00 +0900

    Upload solution

---
+++ Lv0/201 ~ 220/_203[배열 뒤집기]/Soluiton/Solution.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soluiton
+{
+    internal class Solution
+    {
+        public int[] solution(int[] num_list)
+        {
+            Array.Reverse(num_list);
+            return num_list;
+        }
+    }
+}
