c77b3623b94fdcb442dec0a06fee9ab47f2b7abd
OneVth <onebrotravel@gmail.com>
2024-08-20 11:53:29 +0900

    Upload solution

---
+++ Lv0/201 ~ 220/_205[아이스 아메리카노]/Soluiton/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soluiton
+{
+    internal class Solution
+    {
+        public int[] solution(int money)
+        {
+            return new int[2] { money / 5500, money % 5500 };
+        }
+    }
+}
