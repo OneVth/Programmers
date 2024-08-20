e35b5dfbadd4e806f2fe5072d4f76b13f3a758d5
OneVth <onebrotravel@gmail.com>
2024-08-20 11:49:55 +0900

    Upload solution

--- Lv0/201 ~ 220/_201[직각삼각형 출력하기]/Soluiton/Program.cs
+++ Lv0/201 ~ 220/_201[직각삼각형 출력하기]/Soluiton/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            String[] s;
+
+            Console.Clear();
+            s = Console.ReadLine().Split(' ');
+
+            int n = Int32.Parse(s[0]);
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine(new string('*', i));
+            }
         }
     }
 }
