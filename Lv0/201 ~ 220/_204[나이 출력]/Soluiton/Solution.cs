3fcd8624f0741079bcafcb0bcce0274d9443a110
OneVth <onebrotravel@gmail.com>
2024-08-20 11:52:55 +0900

    Upload solution

---
+++ Lv0/201 ~ 220/_204[나이 출력]/Soluiton/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soluiton
+{
+    internal class Solution
+    {
+        public int solution(int age)
+        {
+            return 2022 - age + 1;
+        }
+    }
+}
