49b5b7e0954c8ecc6615fdc4924e5fc57732718a
OneVth <onebrotravel@gmail.com>
2024-08-21 10:51:38 +0900

    Upload solution

---
+++ Lv0/201 ~ 220/_209[피자 나눠 먹기 (2)]/Solution/Solution.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution1(int n)
+        {
+            int answer = 1;
+
+            while (0 != (6 * answer % n))
+                answer++;
+
+            return answer;
+        }
+    }
+}
