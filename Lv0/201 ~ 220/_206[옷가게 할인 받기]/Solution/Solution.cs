39305ee0708fc9ccb8a0c57d8b356b8b29786d2f
OneVth <onebrotravel@gmail.com>
2024-08-21 10:36:23 +0900

    Upload solution

---
+++ Lv0/201 ~ 220/_206[옷가게 할인 받기]/Solution/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(int price)
+        {
+            double[] discountRate = { 0.8, 0.9, 0.95 };
+
+            if (500000 <= price)
+                return (int)(price * discountRate[0]);
+            else if (300000 <= price)
+                return (int)(price * discountRate[1]);
+            else if (100000 <= price)
+                return (int)(price * discountRate[2]);
+            else
+                return price;
+        }
+    }
+}
