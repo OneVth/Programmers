618b123da639391605e45984fc2d3762b002df4f
OneVth <onebrotravel@gmail.com>
2024-08-21 10:37:12 +0900

    Upload solution

---
+++ Lv0/201 ~ 220/_207[배열의 평균값]/Solution/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public double solution(int[] numbers)
+        {
+            return (double)numbers.Sum() / numbers.Length;
+        }
+    }
+}
