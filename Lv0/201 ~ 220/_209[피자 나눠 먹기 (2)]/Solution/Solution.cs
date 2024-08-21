e6c5a411331a496695998c041b18223e45841d12
OneVth <onebrotravel@gmail.com>
2024-08-21 10:51:53 +0900

    Update solution

--- Lv0/201 ~ 220/_209[피자 나눠 먹기 (2)]/Solution/Solution.cs
+++ Lv0/201 ~ 220/_209[피자 나눠 먹기 (2)]/Solution/Solution.cs
@@ -17,5 +17,26 @@ namespace Solution
 
             return answer;
         }
+
+        public int solution2(int n)
+        {
+            return LCM(n, 6) / 6;
+        }
+
+        private int GCD(int a, int b)
+        {
+            while (0 != b)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+
+        private int LCM(int a, int b)
+        {
+            return (a * b) / GCD(a, b);
+        }
     }
 }
