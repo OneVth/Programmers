de080f6554a08d693aa026ebcd2ea9620b1ae57d
OneVth <onebrotravel@gmail.com>
2024-08-23 12:15:44 +0900

    Update solution

--- Lv0/201 ~ 220/_211[짝수는 싫어요]/Solution/Solution.cs
+++ Lv0/201 ~ 220/_211[짝수는 싫어요]/Solution/Solution.cs
@@ -8,9 +8,19 @@ namespace Solution
 {
     internal class Solution
     {
-        public int[] solution(int n)
+        public int[] solution1(int n)
         {
             return Enumerable.Range(1, n).Where(w => w % 2 != 0).ToArray();
+        }
+
+        public int[] solution2(int n)
+        {
+            int[] answer = new int[(n + 1) / 2];
+            for (int i = 0; i < (n + 1) / 2; i++)
+            {
+                answer[i] = (i << 1) + 1;
+            }
+            return answer;
         }
     }
 }
