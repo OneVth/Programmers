489d8ea4e0ecf7b6026c13e535cba71050cf966d
OneVth <onebrotravel@gmail.com>
2024-08-23 12:14:32 +0900

    Upload solution

---
+++ Lv0/201 ~ 220/_211[짝수는 싫어요]/Solution/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int[] solution(int n)
+        {
+            return Enumerable.Range(1, n).Where(w => w % 2 != 0).ToArray();
+        }
+    }
+}
