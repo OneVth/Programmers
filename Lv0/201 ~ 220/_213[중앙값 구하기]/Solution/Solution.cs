4a109a9bc5b54b444011f05e1c6a2ad3bb98576c
OneVth <onebrotravel@gmail.com>
2024-08-23 12:20:14 +0900

    Upload solution

---
+++ Lv0/201 ~ 220/_213[중앙값 구하기]/Solution/Solution.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(int[] array)
+        {
+            Array.Sort(array);
+
+            return array[array.Length / 2]; ;
+        }
+    }
+}
