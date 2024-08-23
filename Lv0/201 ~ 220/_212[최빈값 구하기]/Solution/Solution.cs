9c171fc44424963522e2db07ee0e8271b38542ee
OneVth <onebrotravel@gmail.com>
2024-08-23 12:17:03 +0900

    Upload solution

---
+++ Lv0/201 ~ 220/_212[최빈값 구하기]/Solution/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(int[] array)
+        {
+            var query = array.GroupBy(g => g)
+                .OrderByDescending(o => o.Count())
+                .Take(2)    // 비교를 위해 앞 두개 선택
+                .ToList();
+
+            if (query.Count == 1)
+                return query[0].Key;
+
+            return (query[0].Count() != query[1].Count()) ? query[0].Key : -1;
+        }
+    }
+}
