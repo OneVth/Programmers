a1b2dd118396b4fcdf0e29c8ff97a8b9bbc464b9
OneVth <onebrotravel@gmail.com>
2024-08-23 12:21:43 +0900

    Upload solution

---
+++ Lv0/201 ~ 220/_215[배열 두 배 만들기]/Solution/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int[] solution(int[] numbers)
+        {
+            return numbers.Select(s => s * 2).ToArray();
+        }
+    }
+}
