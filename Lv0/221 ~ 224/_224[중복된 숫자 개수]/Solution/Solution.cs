1ecd7b5db228430af7f3e591b4c901fd2bfa954c
OneVth <onebrotravel@gmail.com>
2024-08-24 11:22:28 +0900

    Upload solution

---
+++ Lv0/221 ~ 224/_224[중복된 숫자 개수]/Solution/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(int[] array, int n)
+        {
+            return array.Count(x => x == n);
+        }
+    }
+}
