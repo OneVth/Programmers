b24ab6153168f4f8823b1dd71eb9504290aec5c3
OneVth <onebrotravel@gmail.com>
2024-08-25 18:00:38 +0900

    Upload solution

---
+++ Lv1/01 ~ 20/_03[나누어 떨어지는 숫자 배열]/Solution/Solution.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int[] solution(int[] arr, int divisor)
+        {
+            int[] answer = new int[] { };
+            answer = arr.Where(w => w % divisor == 0).OrderBy(o => o).ToArray();
+            return answer.Length == 0 ? new int[] { -1 } : answer;
+        }
+    }
+}
