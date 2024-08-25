12dc603162890e47b1c73bda9df37aa4e1d86b36
OneVth <onebrotravel@gmail.com>
2024-08-25 18:05:21 +0900

    Update solution

--- Lv1/01 ~ 20/_04[두 정수 사이의 합]/Solution/Solution.cs
+++ Lv1/01 ~ 20/_04[두 정수 사이의 합]/Solution/Solution.cs
@@ -8,7 +8,7 @@ namespace Solution
 {
     internal class Solution
     {
-        public long solution(int a, int b)
+        public long solution1(int a, int b)
         {
             long answer = 0;
             int min = a < b ? a : b;
@@ -17,6 +17,25 @@ namespace Solution
             for (int i = min; i <= max; i++)
                 answer += i;
             return answer;
+        }
+
+        public long solution2(int a, int b)
+        {
+            long answer = 0;
+
+            while (a != b)
+            {
+                answer += a;
+                a = (a > b) ? a - 1 : a + 1;
+            }
+
+            return answer + b;
+        }
+
+        public long solution3(int a, int b) 
+        {
+            // 등차수열의 합
+            return (((long)Math.Abs(a - b) + 1) * (a + b)) / 2; 
         }
     }
 }
