4fd802952f3e8a9174d4defdff19fb6f4a3e8909
OneVth <onebrotravel@gmail.com>
2024-08-25 18:06:59 +0900

    Upload solution

---
+++ Lv1/01 ~ 20/_05[문자열 내 마음대로 정렬하기]/Solution/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public string[] solution(string[] strings, int n)
+        {
+            return strings.OrderBy(o => o[n]).ThenBy(o => o).ToArray();
+        }
+    }
+}
