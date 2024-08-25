53aa4ca434a828b3b9dd8aa7122a66e5defc37dd
OneVth <onebrotravel@gmail.com>
2024-08-25 17:57:49 +0900

    Update solution

--- Lv1/01 ~ 20/_01[2016년]/Solution/Solution.cs
+++ Lv1/01 ~ 20/_01[2016년]/Solution/Solution.cs
@@ -8,7 +8,7 @@ namespace Solution
 {
     internal class Solution
     {
-        public string solution(int a, int b)
+        public string solution1(int a, int b)
         {
             string answer = "";
             int totalDays = 0;
@@ -45,6 +45,32 @@ namespace Solution
                     answer = "THU";
                     break;
             }
+
+            return answer;
+        }
+
+        public string solution2(int a, int b)
+        {
+            int totalDays = 0;
+            string[] weekName = { "FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU" };
+            int[] nDays = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+            // 1월 1일로부터 지난 일수 구하기
+            for (int i = 0; i < a - 1; i++)
+                totalDays += nDays[i];
+
+            totalDays += b - 1;
+
+            // 요일 구하기
+            return weekName[totalDays % 7];
+        }
+
+        public string solution3(int a, int b)
+        {
+            string answer = "";
+
+            DateTime dateValue = new DateTime(2016, a, b);
+            answer = dateValue.DayOfWeek.ToString().Substring(0, 3).ToUpper();
 
             return answer;
         }
