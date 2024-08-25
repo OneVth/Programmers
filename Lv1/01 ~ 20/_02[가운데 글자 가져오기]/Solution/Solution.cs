9d0e4f6936030602ddb99a2b4b9228f0203e1160
OneVth <onebrotravel@gmail.com>
2024-08-25 17:59:00 +0900

    Upload solution

---
+++ Lv1/01 ~ 20/_02[가운데 글자 가져오기]/Solution/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public string solution(string s)
+        {
+            string answer = "";
+            int length = s.Length;
+
+            if (length % 2 == 0)
+                answer = s.Substring(length / 2 - 1, 2);
+            else
+                answer = s.Substring(length / 2, 1);
+
+            return answer;
+        }
+    }
+}
