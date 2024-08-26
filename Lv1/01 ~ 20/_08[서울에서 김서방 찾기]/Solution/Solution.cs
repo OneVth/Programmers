ce2dedbf9705dc72d59852d9b52656de371d0be9
OneVth <onebrotravel@gmail.com>
2024-08-26 12:54:10 +0900

    Upload solution

---
+++ Lv1/01 ~ 20/_08[서울에서 김서방 찾기]/Solution/Solution.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public string solution(string[] seoul)
+        {
+            int idx = Array.IndexOf(seoul, "Kim");
+            return $"김서방은 {idx}에 있다";
+        }
+    }
+}
