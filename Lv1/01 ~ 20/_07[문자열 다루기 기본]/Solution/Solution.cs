4481d3eae2a0c0c6ab3c2e5c1d6760b3e68e5a5c
OneVth <onebrotravel@gmail.com>
2024-08-26 12:53:23 +0900

    Update solution

--- Lv1/01 ~ 20/_07[문자열 다루기 기본]/Solution/Solution.cs
+++ Lv1/01 ~ 20/_07[문자열 다루기 기본]/Solution/Solution.cs
@@ -2,18 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Solution
 {
     internal class Solution
     {
-        public bool solution(string s)
+        public bool solution1(string s)
         {
             if (s.Length != 4 && s.Length != 6)
                 return false;
 
             return int.TryParse(s, out int result);
+        }
+
+        public bool solution2(string s)
+        {
+            bool answer = false;
+
+            if (s.Length == 4 || s.Length == 6)
+                answer = Regex.IsMatch(s, @"^\d+$");
+
+            return answer;
         }
     }
 }
