564e8381ecad01c179ee4022db50915c7bd8add6
OneVth <onebrotravel@gmail.com>
2024-08-26 12:52:03 +0900

    Upload solution

---
+++ Lv1/01 ~ 20/_07[문자열 다루기 기본]/Solution/Solution.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public bool solution(string s)
+        {
+            if (s.Length != 4 && s.Length != 6)
+                return false;
+
+            return int.TryParse(s, out int result);
+        }
+    }
+}
