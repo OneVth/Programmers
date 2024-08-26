a7dd9c604f1728fc99c9dfd25faaf5b5c5e67f6b
OneVth <onebrotravel@gmail.com>
2024-08-26 12:58:09 +0900

    Upload solution

---
+++ Lv1/01 ~ 20/_10[수박수박수박수박수박수]/Solution/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public string solution(int n)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < n; i++)
+            {
+                if (i % 2 == 0) sb.Append("수");
+                else sb.Append("박");
+            }
+
+            return sb.ToString();
+        }
+    }
+}
