a5ac8fea290741563539cacf2518cada88c39802
OneVth <onebrotravel@gmail.com>
2024-08-27 13:24:10 +0900

    Upload solution

---
+++ Lv1/01 ~ 20/_11[문자열을 정수로 바꾸기]/Solution/Solution.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(string s)
+        {
+            int answer = 0;
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                switch (s[i])
+                {
+                    case '+':
+                        answer += int.Parse(sb.ToString());
+                        sb.Clear();
+                        break;
+                    case '-':
+                        answer -= int.Parse(sb.ToString());
+                        sb.Clear();
+                        break;
+                    default:
+                        sb.Insert(0, s[i]);
+                        break;
+                }
+            }
+
+            // 맨 앞이 부호가 아닐경우 sb 더해주기
+            if (s[0] != '+' && s[0] != '-')
+                answer += int.Parse(sb.ToString());
+
+            return answer;
+        }
+    }
+}
