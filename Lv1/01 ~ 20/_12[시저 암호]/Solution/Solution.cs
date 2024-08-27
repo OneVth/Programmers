58c3598b4778ab25f605cec06a045c3fc0894577
OneVth <onebrotravel@gmail.com>
2024-08-27 13:50:02 +0900

    Upload solution

---
+++ Lv1/01 ~ 20/_12[시저 암호]/Solution/Solution.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public string solution(string s, int n)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (char c in s)
+            {
+                int nc;
+                if (c >= 'A' && c <= 'Z')
+                {
+                    if (c + n > 'Z')
+                        nc = c + n - 26;
+                    else
+                        nc = c + n;
+                }
+                else if (c >= 'a' && c <= 'z')
+                {
+                    if (c + n > 'z')
+                        nc = c + n - 26;
+                    else
+                        nc = c + n;
+                }
+                else
+                    nc = ' ';
+
+                sb.Append((char)nc);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
