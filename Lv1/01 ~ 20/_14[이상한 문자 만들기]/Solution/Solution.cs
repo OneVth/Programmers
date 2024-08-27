a029b7f87cb0a1fc8f174cc539421c51713ed6b2
OneVth <onebrotravel@gmail.com>
2024-08-27 14:36:47 +0900

    Upload solution

---
+++ Lv1/01 ~ 20/_14[이상한 문자 만들기]/Solution/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public string solution(string s)
+        {
+            StringBuilder sb = new StringBuilder();
+            string[] srcArray = s.Split(" ");
+            string[] dstArray = new string[srcArray.Length];
+
+            for (int i = 0; i < srcArray.Length; i++)
+            {
+                for (int j = 0; j < srcArray[i].Length; j++)
+                {
+                    if (j % 2 == 0)
+                        sb.Append(char.ToUpper(srcArray[i][j]));
+                    else
+                        sb.Append(char.ToLower(srcArray[i][j]));
+                }
+                dstArray[i] = sb.ToString();
+                sb.Clear();
+            }
+
+            return String.Join(" ", dstArray);
+        }
+    }
+}
