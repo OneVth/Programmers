fe877db15ed228ea67fa40cfede12c21566b4db7
OneVth <onebrotravel@gmail.com>
2024-08-27 14:25:57 +0900

    Upload solution

---
+++ Lv1/01 ~ 20/_13[약수의 합]/Solution/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(int n)
+        {
+            int answer = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                    answer += i;
+            }
+
+            return answer;
+        }
+    }
+}
