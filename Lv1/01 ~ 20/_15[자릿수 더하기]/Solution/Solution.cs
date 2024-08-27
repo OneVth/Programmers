4a4bcd08b5be47a4844a25fad84a8f945f7fa99a
OneVth <onebrotravel@gmail.com>
2024-08-27 14:39:05 +0900

    Upload solution

---
+++ Lv1/01 ~ 20/_15[자릿수 더하기]/Solution/Solution.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(int n)
+        {
+            int answer = 0;
+
+            foreach (char c in n.ToString())
+                answer += int.Parse(c.ToString());
+
+            return answer;
+        }
+    }
+}
