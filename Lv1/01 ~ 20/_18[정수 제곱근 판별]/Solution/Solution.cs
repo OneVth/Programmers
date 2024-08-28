0b982e7aa3177bbcc94e14b82e35a722eb5b0707
OneVth <onebrotravel@gmail.com>
2024-08-28 12:33:21 +0900

    Upload solution

---
+++ Lv1/01 ~ 20/_18[정수 제곱근 판별]/Solution/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public long solution(long n)
+        {
+            long answer = -1;
+            double sqrt = Math.Sqrt(n);
+
+
+            // for(int i = 1; i * i <= n; i++)
+            // {
+            //     if(n % i == 0)
+            //     {
+            //         if(i * i == n)
+            //         {
+            //             answer = (i + 1) * (i + 1);
+            //             break;
+            //         }
+            //     }
+            // }
+
+            return sqrt % 1 == 0 ? (long)Math.Pow(sqrt + 1, 2) : -1;
+        }
+    }
+}
