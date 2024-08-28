eee26a289e983b25ee2aa9512011c76278815475
OneVth <onebrotravel@gmail.com>
2024-08-28 12:17:06 +0900

    Upload solution

---
+++ Lv1/01 ~ 20/_17[정수 내림차순으로 배치하기]/Solution/Solution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public long solution(long n)
+        {
+            StringBuilder sb = new StringBuilder();
+            string tmp;
+
+            while (n != 0)
+            {
+                sb.Append(n % 10);
+                n /= 10;
+            }
+
+            tmp = sb.ToString();
+            char[] tmpArray = tmp.ToCharArray();
+
+            Array.Sort(tmpArray);
+            Array.Reverse(tmpArray);
+            return long.Parse(new string(tmpArray));
+        }
+    }
+}
