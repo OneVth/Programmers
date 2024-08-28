a3fcba51ac5646b59253f1960927390c9fe2d5e5
OneVth <onebrotravel@gmail.com>
2024-08-28 12:16:20 +0900

    Upload solution

---
+++ Lv1/01 ~ 20/_16[자연수 뒤집어 배열로 만들기]/Solution/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int[] solution(long n)
+        {
+            List<int> list = new List<int>();
+
+            while (n != 0)
+            {
+                list.Add((int)(n % 10));
+                n /= 10;
+            }
+
+            return list.ToArray();
+        }
+    }
+}
