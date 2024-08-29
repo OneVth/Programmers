74ad9113f41c8bb858cd068ebbb6201ae23f4638
OneVth <onebrotravel@gmail.com>
2024-08-29 12:33:33 +0900

    Upload solution

---
+++ Lv1/01 ~ 20/_19[제일 작은 수 제거하기]/Solution/Solution.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int[] solution(int[] arr)
+        {
+            int[] answer = new int[] { };
+            int min = arr.Min();
+
+            answer = arr.Where(w => w != min).ToArray();
+
+            return answer.Length == 0 ? new int[] { -1 } : answer;
+        }
+    }
+}
