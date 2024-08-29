a7ada41ad8da7d1c4e6ff3c077652ae06bf4d253
OneVth <onebrotravel@gmail.com>
2024-08-29 12:35:12 +0900

    Upload solution

---
+++ Lv1/21 ~ 40/_21[최대공약수와 최소공배수]/Solution/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int[] solution(int n, int m)
+        {
+            return new int[] { GCD(n, m), LCM(n, m) };
+        }
+
+        public int GCD(int a, int b)
+        {
+            while (b != 0)
+            {
+                int tmp = b;
+                b = a % b;
+                a = tmp;
+            }
+            return a;
+        }
+
+        public int LCM(int a, int b)
+        {
+            return (a / GCD(a, b)) * b;
+        }
+    }
+}
