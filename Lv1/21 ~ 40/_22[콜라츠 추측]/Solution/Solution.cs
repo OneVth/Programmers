0cec5cb94edbea3a976f3920d3748774bb9dbe05
OneVth <onebrotravel@gmail.com>
2024-08-30 12:02:14 +0900

    Upload solution

---
+++ Lv1/21 ~ 40/_22[콜라츠 추측]/Solution/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(int num)
+        {
+            int answer = -1;
+            long tmp = (long)num;
+
+            int i = 0;
+            while (i < 500)
+            {
+                if (tmp == 1)
+                    return i;
+
+                if (tmp % 2 == 0)
+                    tmp /= 2;
+                else
+                    tmp = tmp * 3 + 1;
+
+                i++;
+            }
+
+            return answer;
+        }
+    }
+}
