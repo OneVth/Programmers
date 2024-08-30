70c4d36ac77cddbbea0c918afafb5e4e1b2add9f
OneVth <onebrotravel@gmail.com>
2024-08-30 12:05:13 +0900

    Upload solution

---
+++ Lv1/21 ~ 40/_24[히샤드 수]/Solution/Solution.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public bool solution(int x)
+        {
+            char[] charArr = x.ToString().ToCharArray();
+            int sum = 0;
+
+            foreach (char c in charArr)
+                sum += int.Parse(c.ToString());
+
+            return x % sum == 0 ? true : false;
+        }
+    }
+}
