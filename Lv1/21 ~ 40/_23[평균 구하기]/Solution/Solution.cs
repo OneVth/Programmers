d8110d431764bb98bd0f6a96bc2609f40fe42f1a
OneVth <onebrotravel@gmail.com>
2024-08-30 12:04:33 +0900

    Upload solution

---
+++ Lv1/21 ~ 40/_23[평균 구하기]/Solution/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public double solution(int[] arr)
+        {
+            return (double)arr.Average();
+        }
+    }
+}
