85388f8dde6b2b3dd6fe83404fb1000bb6c58dc9
OneVth <onebrotravel@gmail.com>
2024-08-31 12:44:43 +0900

    Upload solution

---
+++ Lv1/21 ~ 40/_25[핸드폰 번호 가리기]/Solution/Solution.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public string solution(string phone_number)
+        {
+            string temp = new String('*', phone_number.Length - 4);
+            string lastNumber = phone_number.Substring(phone_number.Length - 4);
+            return String.Concat(temp, lastNumber);
+        }
+    }
+}
