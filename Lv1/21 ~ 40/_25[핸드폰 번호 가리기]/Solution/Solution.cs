47c5e257e46fad8ad5635d1a53f842c1fe82382d
OneVth <onebrotravel@gmail.com>
2024-08-31 13:20:40 +0900

    Update solution

--- Lv1/21 ~ 40/_25[핸드폰 번호 가리기]/Solution/Solution.cs
+++ Lv1/21 ~ 40/_25[핸드폰 번호 가리기]/Solution/Solution.cs
@@ -8,11 +8,16 @@ namespace Solution
 {
     internal class Solution
     {
-        public string solution(string phone_number)
+        public string solution1(string phone_number)
         {
             string temp = new String('*', phone_number.Length - 4);
             string lastNumber = phone_number.Substring(phone_number.Length - 4);
             return String.Concat(temp, lastNumber);
+        }
+
+        public string solution2(string phone_number)
+        {
+            return phone_number.Substring(phone_number.Length - 4).PadLeft(phone_number.Length, '*');
         }
     }
 }
