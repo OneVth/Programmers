c765f9dad4d337a38c648e081dc5cfc8b61f3277
OneVth <onebrotravel@gmail.com>
2024-08-31 13:26:08 +0900

    Upload solution

---
+++ Lv1/21 ~ 40/_27[x만큼 간격이 있는 n개의 숫자]/Solution/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public long[] solution(int x, int n)
+        {
+            return Enumerable.Range(1, n).Select(s => s * (long)x).ToArray();
+        }
+    }
+}
