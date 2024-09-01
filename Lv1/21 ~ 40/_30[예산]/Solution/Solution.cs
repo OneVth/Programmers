59b3cb9377e1b70fb68c125b1d5d13ad4c37e45f
OneVth <onebrotravel@gmail.com>
2024-09-01 13:05:01 +0900

    Upload solution

---
+++ Lv1/21 ~ 40/_30[예산]/Solution/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(int[] d, int budget)
+        {
+            int answer = 0;
+            int[] ordered = d.OrderBy(o => o).ToArray();
+
+            for (int i = 0; i < d.Length; i++)
+            {
+                if (budget >= ordered[i])
+                {
+                    budget -= ordered[i];
+                    answer++;
+                    if (budget == 0)
+                        break;
+                }
+                else
+                    continue;
+            }
+
+            return answer;
+        }
+    }
+}
