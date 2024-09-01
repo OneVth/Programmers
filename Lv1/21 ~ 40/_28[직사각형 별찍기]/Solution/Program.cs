004c6ae8e5fc1a79220c271035e355673ed5f754
OneVth <onebrotravel@gmail.com>
2024-09-01 12:51:01 +0900

    Upload solution

--- Lv1/21 ~ 40/_28[직사각형 별찍기]/Solution/Program.cs
+++ Lv1/21 ~ 40/_28[직사각형 별찍기]/Solution/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            String[] s;
+
+            Console.Clear();
+            s = Console.ReadLine().Split(' ');
+
+            int a = Int32.Parse(s[0]);
+            int b = Int32.Parse(s[1]);
+
+            for(int i = 0; i < b; i++)
+            {
+                for(int j = 0; j < a; j++)
+                {
+                    Console.Write('*');
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
