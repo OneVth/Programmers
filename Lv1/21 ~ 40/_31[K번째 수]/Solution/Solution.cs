b61ce6cd112295f828903bad7ef7923715e9d37b
OneVth <onebrotravel@gmail.com>
2024-09-02 14:27:14 +0900

    Update solution

--- Lv1/21 ~ 40/_31[K번째 수]/Solution/Solution.cs
+++ Lv1/21 ~ 40/_31[K번째 수]/Solution/Solution.cs
@@ -8,7 +8,7 @@ namespace Solution
 {
     internal class Solution
     {
-        public int[] solution(int[] array, int[,] commands)
+        public int[] solution1(int[] array, int[,] commands)
         {
             int i, j, k;
             int length;
@@ -24,6 +24,25 @@ namespace Solution
 
                 Array.Copy(array, i, temp, 0, length);
                 answer[r] = temp.OrderBy(o => o).ToArray()[k];
+            }
+
+            return answer;
+        }
+
+        public int[] solution2(int[] array, int[,] commands)
+        {
+            int i, j, k;
+            int length;
+            int[] answer = new int[commands.GetLength(0)];
+
+            for(int r = 0; r < commands.GetLength(0); r++)
+            {
+                i = commands[r, 0] - 1;
+                j = commands[r, 1] - 1;
+                k = commands[r, 2] - 1;
+                length = j - i + 1;
+
+                answer[r] = array.Skip(i).Take(length).OrderBy(o => o).ToArray()[k];
             }
 
             return answer;
