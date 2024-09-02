9356724124ea530fdffd99cd278392d8ad16c548
OneVth <onebrotravel@gmail.com>
2024-09-02 14:43:50 +0900

    Update solution

--- Lv1/21 ~ 40/_33[체육복]/Solution/Solution.cs
+++ Lv1/21 ~ 40/_33[체육복]/Solution/Solution.cs
@@ -8,7 +8,7 @@ namespace Solution
 {
     internal class Solution
     {
-        public int solution(int n, int[] lost, int[] reserve)
+        public int solution1(int n, int[] lost, int[] reserve)
         {
             // 학생 번호와 index 대응을 위해 n + 2개의 배열을 만듦
             // 즉, 0번, 마지막 인덱스는 Dummy
@@ -38,6 +38,28 @@ namespace Solution
             }
 
             return nTraining.Count(x => x >= 1) - 2; // 더미의 개수 제거
+        }
+
+        public int solution2(int n, int[] lost, int[] reserve)
+        {
+            // Convert arrays to lists for easier manipulation
+            List<int> lostList = lost.ToList();
+            List<int> reserveList = reserve.ToList();
+
+            // Remove students who are both in lost and reserve lists
+            List<int> finalLost = lostList.Except(reserveList).ToList();
+            List<int> finalReserve = reserveList.Except(lostList).ToList();
+
+            foreach (int student in finalReserve)
+            {
+                if (finalLost.Contains(student - 1))
+                    finalLost.Remove(student - 1);
+                else if (finalLost.Contains(student + 1))
+                    finalLost.Remove(student + 1);
+            }
+
+            // The number of students who can attend the gym class
+            return n - finalLost.Count;
         }
     }
 }
