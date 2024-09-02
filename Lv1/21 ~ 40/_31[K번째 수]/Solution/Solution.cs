5954534718af3d5258fcb194532d885d3bff2930
OneVth <onebrotravel@gmail.com>
2024-09-02 14:18:25 +0900

    Upload solution

---
+++ Lv1/21 ~ 40/_31[K번째 수]/Solution/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int[] solution(int[] array, int[,] commands)
+        {
+            int i, j, k;
+            int length;
+            int[] answer = new int[commands.GetLength(0)];
+
+            for (int r = 0; r < commands.GetLength(0); r++)
+            {
+                i = commands[r, 0] - 1;
+                j = commands[r, 1] - 1;
+                k = commands[r, 2] - 1;
+                length = j - i + 1;
+                int[] temp = new int[length];
+
+                Array.Copy(array, i, temp, 0, length);
+                answer[r] = temp.OrderBy(o => o).ToArray()[k];
+            }
+
+            return answer;
+        }
+    }
+}
