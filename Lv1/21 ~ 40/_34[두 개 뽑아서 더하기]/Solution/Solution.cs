45b76ae9b691417b20309303004429c543c1dbe0
OneVth <onebrotravel@gmail.com>
2024-09-04 13:53:12 +0900

    Upload solution

---
+++ Lv1/21 ~ 40/_34[두 개 뽑아서 더하기]/Solution/Solution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int[] solution(int[] numbers)
+        {
+            List<int> list = new List<int>();
+
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    list.Add(numbers[i] + numbers[j]);
+                }
+            }
+
+            return list.OrderBy(o => o).Distinct().ToArray();
+        }
+    }
+}
