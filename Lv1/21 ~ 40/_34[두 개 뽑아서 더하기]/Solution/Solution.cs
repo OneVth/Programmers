ee400bd4cb5f80a4d4a98b6f262df93b0bded48b
OneVth <onebrotravel@gmail.com>
2024-09-04 13:55:21 +0900

    Update solution

--- Lv1/21 ~ 40/_34[두 개 뽑아서 더하기]/Solution/Solution.cs
+++ Lv1/21 ~ 40/_34[두 개 뽑아서 더하기]/Solution/Solution.cs
@@ -8,7 +8,7 @@ namespace Solution
 {
     internal class Solution
     {
-        public int[] solution(int[] numbers)
+        public int[] solution1(int[] numbers)
         {
             List<int> list = new List<int>();
 
@@ -21,6 +21,24 @@ namespace Solution
             }
 
             return list.OrderBy(o => o).Distinct().ToArray();
+        }
+
+        public int[] solution2(int[] numbers)
+        {
+            List<int> answerList = new List<int>();
+
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (!answerList.Contains(numbers[i] + numbers[j]))
+                        answerList.Add(numbers[i] + numbers[j]);
+                }
+            }
+
+            answerList.Sort();
+
+            return answerList.ToArray();
         }
     }
 }
