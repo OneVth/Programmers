234773c1646ef9488f9c37cf2d6b9482665cacbe
OneVth <onebrotravel@gmail.com>
2024-09-04 13:56:12 +0900

    Upload solution

---
+++ Lv1/21 ~ 40/_35[3진법 뒤집기]/Solution/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(int n)
+        {
+            int answer = 0;
+            int length = 0;
+
+            // 3진법 길이 구하기
+            while (n >= Math.Pow(3, length))
+            {
+                length++;
+            }
+
+            // 3진법으로 변환 + 앞뒤 반전 + 10진법으로 변환 동시에
+            for (int i = length - 1, j = 0; i >= 0; i--, j++)
+            {
+                answer += n / (int)Math.Pow(3, i) * (int)Math.Pow(3, j);
+                n = n % (int)Math.Pow(3, i);
+            }
+
+            return answer;
+        }
+    }
+}
