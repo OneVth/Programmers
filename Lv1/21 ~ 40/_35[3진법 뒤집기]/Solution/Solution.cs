ac64195942ceea043c8292b3d33e18b027ba10bb
OneVth <onebrotravel@gmail.com>
2024-09-04 14:17:17 +0900

    Update solution

--- Lv1/21 ~ 40/_35[3진법 뒤집기]/Solution/Solution.cs
+++ Lv1/21 ~ 40/_35[3진법 뒤집기]/Solution/Solution.cs
@@ -22,7 +22,8 @@ namespace Solution
             // 3진법으로 변환 + 앞뒤 반전 + 10진법으로 변환 동시에
             for (int i = length - 1, j = 0; i >= 0; i--, j++)
             {
-                answer += n / (int)Math.Pow(3, i) * (int)Math.Pow(3, j);
+                int currentDigit = n / (int)Math.Pow(3, i);
+                answer += currentDigit * (int)Math.Pow(3, j);
                 n = n % (int)Math.Pow(3, i);
             }
 
