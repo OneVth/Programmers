887fff23ded1eabecf2aa48a46ebb7c233bb3842
OneVth <onebrotravel@gmail.com>
2024-09-05 13:25:04 +0900

    Upload solution

---
+++ Lv1/21 ~ 40/_38[로또의 최고 순위와 최저 순위]/Solution/Solution.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int[] solution(int[] lottos, int[] win_nums)
+        {
+            int[] ranking = { 6, 6, 5, 4, 3, 2, 1 };
+            int correctCount = 0;
+            int zeroCount = lottos.Count(x => x == 0);
+
+            IEnumerable<int> notCorrected = win_nums.Except(lottos);
+
+            // 모두 일치하는 경우
+            if (notCorrected.Count() == 0)
+                return new int[] { 1, 1 };
+
+            correctCount = win_nums.Length - notCorrected.Count();
+
+            int maxCorrect = correctCount + zeroCount; // 최대 일치 개수
+            int minCorrect = correctCount; // 최소 일치 개수
+
+            return new int[] { ranking[maxCorrect], ranking[minCorrect] };
+        }
+    }
+}
