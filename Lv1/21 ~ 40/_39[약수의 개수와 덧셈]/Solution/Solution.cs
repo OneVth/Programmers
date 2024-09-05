807881cf86e47c6885df6a233eb6f973e1fe4d17
OneVth <onebrotravel@gmail.com>
2024-09-05 13:42:24 +0900

    Upload solution

---
+++ Lv1/21 ~ 40/_39[약수의 개수와 덧셈]/Solution/Solution.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(int left, int right)
+        {
+            int answer = 0;
+
+            for (int i = left; i <= right; i++)
+            {
+                answer += (GetDivisorCount(i) % 2) == 0 ? i : -i;
+            }
+
+            return answer;
+        }
+
+        int GetDivisorCount(int number)
+        {
+            int count = 0;
+            for (int i = 1; i <= number; i++)
+            {
+                if (number % i == 0)
+                    count++;
+            }
+            return count;
+        }
+    }
+}
