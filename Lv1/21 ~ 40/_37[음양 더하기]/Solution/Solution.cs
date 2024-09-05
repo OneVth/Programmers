74224dfbba13d752a6f2b52ceaf311d87bde125e
OneVth <onebrotravel@gmail.com>
2024-09-05 13:21:39 +0900

    Upload solution

---
+++ Lv1/21 ~ 40/_37[음양 더하기]/Solution/Solution.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(int[] absolutes, bool[] signs)
+        {
+            int answer = 0;
+
+            for (int i = 0; i < signs.Length; i++)
+            {
+                answer += signs[i] ? absolutes[i] : -absolutes[i];
+            }
+
+            return answer;
+        }
+    }
+}
