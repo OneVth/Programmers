de49c89a9e537e95136f2edb3c81c1fea02b5be0
OneVth <onebrotravel@gmail.com>
2024-09-05 13:50:35 +0900

    Update solution

--- Lv1/21 ~ 40/_39[약수의 개수와 덧셈]/Solution/Solution.cs
+++ Lv1/21 ~ 40/_39[약수의 개수와 덧셈]/Solution/Solution.cs
@@ -30,5 +30,20 @@ namespace Solution
             }
             return count;
         }
+
+        int CountDivisors(int number)
+        {
+            int count = 0;
+            for(int i = 1; i * i <= number; i++)
+            {
+                if(number % i == 0)
+                {
+                    count++;
+                    if (i != number / i)
+                        count++;
+                }    
+            }
+            return count;
+        }
     }
 }
