6452a2b8734f8ca6bd0d84744a9f92d0c1489dd6
OneVth <onebrotravel@gmail.com>
2024-09-06 12:09:29 +0900

    Upload solution

---
+++ Lv1/21 ~ 40/_40[숫자 문자열과 영단어]/Solution/Solution.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Solution
+{
+    public class Solution
+    {
+        public int solution(string s)
+        {
+            string[] words = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (s.Contains(words[i]))
+                    s = s.Replace(words[i], i.ToString());
+            }
+
+            return int.Parse(s);
+        }
+    }
+}
