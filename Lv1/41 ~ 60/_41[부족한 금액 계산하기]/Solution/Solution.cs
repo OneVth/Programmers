51529a7bd69180cb56f1637bef885f6370d03d82
OneVth <onebrotravel@gmail.com>
2024-09-06 12:19:25 +0900

    Upload solution

---
+++ Lv1/41 ~ 60/_41[부족한 금액 계산하기]/Solution/Solution.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public long solution(int price, int money, int count)
+        {
+            // a = price, d = price인 등차수열
+            long reqPrice = 0;
+            long totPrice = 0;
+
+            totPrice = (((long)count + 1) * price * count) / 2;
+            reqPrice = totPrice - money;
+
+            return reqPrice > 0 ? reqPrice : 0;
+        }
+    }
+}
