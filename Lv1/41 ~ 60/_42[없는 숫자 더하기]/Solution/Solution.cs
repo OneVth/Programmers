c60e67d41569ccac408700fcd9d529afb9a6c803
OneVth <onebrotravel@gmail.com>
2024-09-06 12:20:17 +0900

    Upload solution

---
+++ Lv1/41 ~ 60/_42[없는 숫자 더하기]/Solution/Solution.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(int[] numbers)
+        {
+            int[] cmpArray = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            return cmpArray.Except(numbers).Sum();
+        }
+    }
+}
