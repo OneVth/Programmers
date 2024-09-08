4c0ca9034a13f7d5cef744cf92bd0a9bf702f900
OneVth <onebrotravel@gmail.com>
2024-09-08 14:21:44 +0900

    Upload solution

---
+++ Lv1/41 ~ 60/_43[최소 직사각형]/Solution/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(int[,] sizes)
+        {
+            int width = 0;
+            int height = 0;
+
+            for (int i = 0; i < sizes.GetLength(0); i++)
+            {
+                int longer = sizes[i, 0] > sizes[i, 1] ? sizes[i, 0] : sizes[i, 1];
+                int shorter = sizes[i, 0] > sizes[i, 1] ? sizes[i, 1] : sizes[i, 0];
+
+                width = longer > width ? longer : width;
+                height = shorter > height ? shorter : height;
+            }
+
+            return width * height;
+        }
+    }
+}
