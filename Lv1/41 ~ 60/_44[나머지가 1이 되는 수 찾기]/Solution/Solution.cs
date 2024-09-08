ceee1bcf0c5f298497f5f08193ca24744e0eb609
OneVth <onebrotravel@gmail.com>
2024-09-08 14:25:41 +0900

    Upload solution

---
+++ Lv1/41 ~ 60/_44[나머지가 1이 되는 수 찾기]/Solution/Solution.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(int n)
+        {
+            for (int i = 2; i < n; i++)
+            {
+                if (n % i == 1)
+                    return i;
+            }
+            return 0;
+        }
+    }
+}
