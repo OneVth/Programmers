7ce61bb6abbc4df53c0d665551c698a37dc9aa40
OneVth <onebrotravel@gmail.com>
2024-09-09 14:46:02 +0900

    Upload solution

---
+++ Lv1/41 ~ 60/_48[삼총사]/Solution/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(int[] number)
+        {
+            int answer = 0;
+            for (int i = 0; i < number.Length - 2; i++)
+            {
+                for (int j = i + 1; j < number.Length - 1; j++)
+                {
+                    for (int k = j + 1; k < number.Length; k++)
+                    {
+                        if (number[i] + number[j] + number[k] == 0)
+                            answer++;
+                    }
+                }
+            }
+            return answer;
+        }
+    }
+}
