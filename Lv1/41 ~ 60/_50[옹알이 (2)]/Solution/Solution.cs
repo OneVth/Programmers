66e8f71f2d40b8180b8ce906eaac14a73d143a60
OneVth <onebrotravel@gmail.com>
2024-09-10 13:18:17 +0900

    Upload solution

---
+++ Lv1/41 ~ 60/_50[옹알이 (2)]/Solution/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(string[] babbling)
+        {
+            int answer = 0;
+            string[] canPronounce = { "aya", "ye", "woo", "ma" };
+
+            foreach (string s in babbling)
+            {
+                string temp = s;
+                foreach (string pronounce in canPronounce)
+                {
+                    if (temp.Contains(pronounce + pronounce)) // 연속 중복 확인
+                        continue;
+
+                    temp = temp.Replace(pronounce, " ");
+                }
+                if (string.IsNullOrWhiteSpace(temp))
+                    answer++;
+            }
+            return answer;
+        }
+    }
+}
