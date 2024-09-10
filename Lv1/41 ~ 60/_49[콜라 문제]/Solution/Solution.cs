aaa56863ed72ad1874ccf31165dd2bbd9caf763c
OneVth <onebrotravel@gmail.com>
2024-09-10 13:17:07 +0900

    Upload solution

---
+++ Lv1/41 ~ 60/_49[콜라 문제]/Solution/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(int a, int b, int n)
+        {
+            int answer = 0;
+
+            while (n >= a)
+            {
+                int temp = n / a * b;
+                n = n % a + temp;
+
+                answer += temp;
+            }
+            return answer;
+        }
+    }
+}
