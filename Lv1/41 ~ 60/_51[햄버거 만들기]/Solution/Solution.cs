13bdf19bbda94f491efe3c461b7271cf61ac3617
OneVth <onebrotravel@gmail.com>
2024-09-11 12:51:13 +0900

    Upload solution

---
+++ Lv1/41 ~ 60/_51[햄버거 만들기]/Solution/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    internal class Solution
+    {
+        public int solution(int[] ingredient)
+        {
+            int answer = 0;
+            Stack<int> stack = new Stack<int>();
+
+            foreach (int element in ingredient)
+            {
+                stack.Push(element);
+
+                if (stack.Count >= 4 && stack.Take(4).SequenceEqual(new List<int> { 1, 3, 2, 1 }))
+                {
+                    for (int i = 0; i < 4; i++)
+                    {
+                        stack.Pop();
+                    }
+                    answer++;
+                }
+            }
+
+            return answer;
+        }
+    }
+}
