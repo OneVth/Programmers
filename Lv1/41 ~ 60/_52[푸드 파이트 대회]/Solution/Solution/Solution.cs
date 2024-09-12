b21c9d4e31221b0555adb86c8bfaca7ea53f1bd2
OneVth <onebrotravel@gmail.com>
2024-09-12 13:15:30 +0900

    Upload solution

--- Lv1/41 ~ 60/_52[푸드 파이트 대회]/Solution/Solution/Solution.cs
+++ Lv1/41 ~ 60/_52[푸드 파이트 대회]/Solution/Solution/Solution.cs
@@ -8,5 +8,25 @@ namespace Solution
 {
     internal class Solution
     {
+        public string solution(int[] food)
+        {
+            string[] chunk = new string[2];
+            StringBuilder temp = new StringBuilder();
+
+            for (int i = 1; i < food.Length; i++)
+            {
+                for (int j = 0; j < food[i] / 2; j++)
+                {
+                    temp.Append(i);
+                }
+            }
+
+            chunk[0] = temp.ToString();
+            char[] cArray = chunk[0].ToCharArray();
+            Array.Reverse(cArray);
+            chunk[1] = new string(cArray);
+
+            return string.Join("0", chunk);
+        }
     }
 }
